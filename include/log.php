6d739f854ef1a6094aca9ae2d9a18f317dc5b7ad
Przemek Sosnowski <sosprz@gmail.com>
2012-08-20 12:39:02 +0200

    add clear log

--- include/log.php
+++ include/log.php
@@ -1,4 +1,15 @@
+<?php	
+	if ($_POST['log_del'] == "Clear"){
+	 exec("rm scripts/tmp/log.txt");	
+	 header("location: " . $_SERVER['REQUEST_URI']);
+	 exit();
+	 } 
+	 ?>	
+
+
 <div id="left"><span class="belka">&nbsp Log: <span class="okno">
+<form action="log" method="post"><input type="submit" name="log_del" value="Clear" /></form>
+<br />
 <pre>
 <?php
 include('scripts/tmp/log.txt');
