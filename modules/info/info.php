6d2c2185512e6df2bf372ead5c9be3e2cb9b541a
sosprz <root@raspberrypi.(none)>
2013-05-15 14:03:34 +0200

    remove include folder

---
+++ modules/info/info.php
@@ -0,0 +1,14 @@
+<div id="left"><span class="belka">&nbsp info: <span class="okno">
+Contact:  </br >
+http://techfreak.pl/nettemp  </br >
+techfreak.pl@gmail.com
+
+<br />
+<br />
+Donate for developing:<br /><br />
+<?php
+include('paypal.php');
+?>
+
+</span></span>
+</div>
---
+++ modules/login/login.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+$inactive = 100;
+if (isset($_SESSION["timeout"])) {
+       $sessionTTL = time() - $_SESSION["timeout"];
+    if ($sessionTTL > $inactive) {
+        session_destroy();
+       }
+}
+$_SESSION["timeout"] = time();
+?>
+ 
+<?php
+if ($_POST['form_logout'] == "log") { session_destroy(); header("location: ".$_SERVER['PHP_SELF']);}
+
+if ($_POST["form_login"]=="log") { /// do after login form is submitted  
+	$user=$_POST["username"];
+	$pass=sha1($_POST["password"]);
+	$db = new PDO('sqlite:dbf/nettemp.db');
+	$rows = $db->query("SELECT * FROM users WHERE login='$user' AND password='$pass' ");
+	$row = $rows->fetchAll();
+   $numRows = count($row);
+     if ($numRows == 1) { $_SESSION["logged"]=$_POST["username"];    
+     session_regenerate_id();
+     } 
+     else {echo 'Wrong login or password, try again.';}; 
+	}; 
+
+	include("login_check.php");
+	if ($numRows1 == 1) { 	?>
+	<span class="login">	
+	<form action="index.php" method="post">
+	<input type="hidden" name="form_logout" value="log"> 
+	<?php echo $logged; ?>
+  <input type="image" src="media/ico/Lock-icon.png" type="submit" value="Logout" />
+	</form> 	
+	</span>
+	<?php	 
+	}
+	  else { ?>
+	  <span class="login">
+	  <form action="index.php" method="post">
+	  <input type="hidden" name="form_login" value="log">  
+     Username: <input type="text" name="username" size="8"/><br>
+     Password: <input type="password" name="password" size="8"/><br>
+     <input type="image" src="media/ico/Unlock-icon.png" type="submit" value="Login" />
+     </form>
+     </span>
+	<?php	}; 
+?>
