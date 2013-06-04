9142eb27fbeff6e1afbc5b416661adadd0aee66f
root <root@raspberrypi.(none)>
2013-06-04 23:30:35 +0200

    change view to simple

--- modules/view/html/view.php
+++ modules/view/html/view.php
@@ -1,11 +1,9 @@
 <div id="left">
-<?php include("modules/view/html/view_del.php"); ?>
 <?php
 		session_start(); 
 		include('include/login_check.php');
 
 if ($numRows1 == 1 && ($perms == "ops" || $perms == "adm" )) { include("include/view_create.php"); }; 
 ?>
-<?php //include("modules/view/html/view_select_sensors.php"); ?>
 <?php include("modules/view/html/view_graph.php"); ?>
 </DIV>
--- modules/view/html/view_graph.php
+++ modules/view/html/view_graph.php
@@ -1,26 +1,15 @@
-<span class="belka">&nbsp Graph<span class="okno">
+<span class="belka">&nbsp Graphing<span class="okno">
 <table> 
 <?php
 session_start();
 $arrayFiles=glob('img/view1/*');
 if($arrayFiles){
   foreach ($arrayFiles as $filename) { ?>
-<tr><td>
-<span class="pasek_wykres"><table> <tr><td align="left">&nbsp</td>
-<td align="right" width="980">
+<table> <tr><td>
 <?php	 
 include("include/login_check.php");
-if ($numRows1 == 1) { ?><form action="view" method="post">
-<?php $zm2=substr($filename ,10,-4); ?>
-<input type="hidden" name="del_rrd" value="<?php echo "$zm2"; ?>" />
-<input type="hidden" name="del_graf" value="<?php echo "$filename"; ?>" />
-<input type="hidden" name="del_graf1" value="del_graf2" />
-<input type="image" src="media/ico/Close-icon.png" class="del_graf"  /></td></tr>
-</form> 
-<?php }; ?>
-</table>
+?>
 <img src="<?php echo $filename; ?>" />
-</span>
 </td></tr>
 <?php
  }} else { ?>
