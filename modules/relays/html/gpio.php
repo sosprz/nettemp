f692fdf1460563026c7f641c8e2d935e103e20b8
techfreak.pl <techfreak.pl@gmail.com>
2013-06-10 22:30:18 +0200

    relays submit

--- modules/relays/html/gpio.php
+++ modules/relays/html/gpio.php
@@ -75,32 +75,30 @@ exec("$dir/gpio onoff $gpio", $out_arr);
 	<td>	<img type="image" src="media/ico/SMD-64-pin-icon_24.png" /></td>
 	<td><?php echo $a['name']; ?></td>
 	<input type="hidden" name="gpio" value="<?php echo "$gpio"; ?> "/>
-	<td><input type="image" name="off" value="OFF" src="media/ico/Button-Turn-Off-icon.png"/></td>
+	<input type="hidden" name="off" value="OFF" />
+	<td><input type="image"  src="media/ico/Button-Turn-Off-icon.png"/></td>
 	</form>
 	<td><?php passthru("$dir/gpio check $gpio");?> </td>
 	</tr></table>
  <?php	} elseif ($out == 'off') { ?>
 	<table>
 	<form action="relays" method="post">
-	<td><img src="media/ico/SMD-64-pin-icon_24.png" /></td>
-	<td><input type="text" name="name" value="<?php echo $a['name']; ?>" size="10" /></td>
-	<input type="hidden" name="name1" value="name2"/>
-	<input type="hidden" name="name_id" value="<?php echo $a['id']; ?>" />
-	<td><input type="image" src="media/ico/Actions-edit-redo-icon.png" /></td>
+	<td><img type="image" src="media/ico/SMD-64-pin-icon_24.png" ></td>
+	<td><input type="text" name="name" value="<?php echo $a['name']; ?>" size="10"></td>
+	<input type="hidden" name="name1" value="name2">
+	<input type="hidden" name="name_id" value="<?php echo $a['id']; ?>" >
+	<td><input type="image" src="media/ico/Actions-edit-redo-icon.png" alt="Submit" ></td>
 	</form>
 	<form action="relays" method="post">
-	<td><img type="image" src="media/ico/Clock-icon.png" /></td>
-	<td><input type="checkbox" name="custom_time_on" value="on" <?php echo $a["custom_time_on"] == 'on' ? 'checked="checked"' : ''; ?>  onclick="this.form.elements['timecm'].disabled = !this.checked" /><td>
-	<td><input type="text" name="timecm" value="<?php echo $a['custom_time']/60; ?>" size="5" disabled="disabled" /></td><td>min</td> 
+	<td><img  src="media/ico/Clock-icon.png" /></td>
+	<td><input type="checkbox" name="custom_time_on" value="on" <?php echo $a["custom_time_on"] == 'on' ? 'checked="checked"' : ''; ?>  onclick="this.form.elements['timecm'].disabled = !this.checked" ><td>
+	<td><input type="text" name="timecm" value="<?php echo $a['custom_time']/60; ?>" size="5" disabled="disabled" ></td><td>min</td> 
 	<input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
-	<td><input type="image" name="on" value="ON" src="media/ico/Button-Turn-On-icon.png"/></td>
+	<input type="hidden" name="on" value="ON" />
+	<td><input type="image" src="media/ico/Button-Turn-On-icon.png"/></td>
 	</form>
 	</tr>
 	</table>
-
-
-
-
 
 <?php } ?>
 	</span></span>
