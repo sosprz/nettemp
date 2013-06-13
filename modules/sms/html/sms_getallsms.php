f0795d56a8231042bc9a1c23f6ef22cc0f527849
sosprz <techfreak.pl@gmail.com>
2013-06-14 00:09:11 +0200

    get sms

---
+++ modules/sms/html/sms_getallsms.php
@@ -0,0 +1,31 @@
+<?php
+        if ($_POST['getallsms'] == "Get all sms"){
+         exec("gammu -c tmp/gammurc getallsms > tmp/gammu_getallsms");  
+         header("location: " . $_SERVER['REQUEST_URI']);
+         exit();
+         } 
+        if ($_POST['delallsms'] == "Del all sms"){
+         exec("gammu -c tmp/gammurc deleteallsms 1"); 
+         exec("gammu -c tmp/gammurc getallsms > tmp/gammu_getallsms");
+         //unset("tmp/gammu_getallsms");
+         header("location: " . $_SERVER['REQUEST_URI']);
+         exit();
+         } 
+         
+
+?>
+<table><tr>
+<form action="sms" method="post">
+<td><input type="submit" name="getallsms" value="Get all sms" /></td>
+</form>
+<form action="sms" method="post">
+<td><input type="submit" name="delallsms" value="Del all sms" /></td>
+</form>
+</tr></table>
+
+<div style="width:990px;height:300px;overflow:auto;padding:5px;">
+<pre><?php include('tmp/gammu_getallsms'); ?></pre>
+</div>
+
+
+
