41dc3da7b1ac5164af7358905d225ca9a8dd5644
root <root@raspberrypi.(none)>
2013-06-16 20:11:20 +0000

    version bump

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ case 'alarms': include('modules/notification/html/notification.php'); break;
 
 </div>
 
-	<div id="footer">www.nettemp.pl v7
+	<div id="footer">www.nettemp.pl v7.1
 	</div>
 </div>
 
