4eecb0a20f61d2a67fdb6a540ced743f2cf07823
root <root@raspberrypi.(none)>
2013-07-21 05:54:35 +0000

    change branch name && new installation script

--- nettemp_install_on_raspberry_pi.sh
+++ nettemp_install_on_raspberry_pi.sh
@@ -10,8 +10,8 @@ if [[ $UID -ne 0 ]]; then
 fi 
 
 echo "update distro"
- apt-get update
- apt-get -y upgrade
+# apt-get update
+# apt-get -y upgrade
 
 echo "install git-core"
  apt-get -y install git-core
@@ -26,12 +26,29 @@ echo "enable module: fastcgi-php"
 echo "changing lighthttpd conf"
  sed -i -e 's/#       "mod_rewrite",/       "mod_rewrite",/g'  /etc/lighttpd/lighttpd.conf
  sed -i -e 's/server.document-root        = \"\/var\/www\"/server.document-root        = \"\/var\/www\/nettemp\"/g'  /etc/lighttpd/lighttpd.conf	
+
+ if cat /etc/lighttpd/lighttpd.conf |grep url.rewrite-once 
+ then
+ echo "Url rewrite exist"
+ else
  echo "url.rewrite-once = ( \"^/([A-Za-z0-9-_-]+)\$\" => \"/index.php?id=\$1\" )" >> /etc/lighttpd/lighttpd.conf
+ fi
 
 
-echo "downloading nettemp source"
- cd /var/www
- git clone https://github.com/sosprz/nettemp
+echo "Which version you want to download"
+echo "Regular [r] Beta [b]"
+read x 
+cd /var/www
+if [ x == ] then
+git clone https://github.com/sosprz/nettemp
+fi
+
+if [ x == ] then
+git clone https://github.com/sosprz/nettemp
+fi
+
+
+
 
 echo "permisions"
  chown -R root.www-data /var/www/nettemp
@@ -50,6 +67,7 @@ echo "add cron"
 
  update-rc.d ntp enable
  service ntp start
+
  update-rc.d lighttpd enable
  service lighttpd stop
  service lighttpd start
@@ -58,24 +76,33 @@ echo "add cron"
  service cron start
 
 echo "add wiringPI for gpio"
+ if which gpio 
+ then echo "wiringPI exist"
+ else
  git clone git://git.drogon.net/wiringPi
  cd wiringPi
  ./build
-
+ fi
 
 echo "add watchdog"
- echo "bcm2708_wdog" | sudo tee -a /etc/modules
+ 
  apt-get install watchdog
  update-rc.d watchdog defaults
+ 
+ if cat /etc/modules |grep bcm2708_wdog
+ then echo "bcm2708_wdog exist in file"
+ else echo "bcm2708_wdog" | sudo tee -a /etc/modules
+ fi
  sed -i -e '10s/#max-load-1/max-load-1/' /etc/watchdog.conf 
  sed -i -e '23s/#watchdog-device/watchdog-device/' /etc/watchdog.conf 
-
  /etc/init.d/watchdog start
 
 echo "add modules 1-wire"
- echo "w1_gpio" | sudo tee -a /etc/modules
- echo "w1_therm" | sudo tee -a /etc/modules
-
+ if cat /etc/modules |grep w1_
+ then echo "1-wire modules exist in file"
+ else  echo "w1_gpio" | sudo tee -a /etc/modules
+       echo "w1_therm" | sudo tee -a /etc/modules
+ fi
 echo "restart RPI to make sure everything is ok"
 
 
