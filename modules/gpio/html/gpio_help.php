b6d078d97b39a2088fbbbd46634a3228a214a266
sosprz <root@raspberrypi.(none)>
2013-08-05 09:20:39 +0000

    gpio - add temp function, change time function - fixes

--- modules/gpio/html/gpio_help.php
+++ modules/gpio/html/gpio_help.php
@@ -1,5 +1,3 @@
 <span class="belka">&nbsp Help<span class="okno">
-In this section You can add function to You Gpio pin. 
-For now you can specify how long to GPIO will be in HIGH state.
-This is a good use for the relays.
+In this section You can add Relay function (ON/OFF) to You Gpio pin. After add, Gpio have OUTPUT mode.
 </span></span>
