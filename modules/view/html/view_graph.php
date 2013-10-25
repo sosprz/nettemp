4e961becc121aee1be477f43e2686c621ce58f19
sosprz <root@raspberrypi.(none)>
2013-10-25 21:28:34 +0000

    add sort to RRD view

--- modules/view/html/view_graph.php
+++ modules/view/html/view_graph.php
@@ -1,19 +1,26 @@
 <span class="belka">&nbsp RRD<span class="okno">
-<table> 
+
 <?php
 include("modules/login/login_check.php");
 
 $arrayFiles=glob('img/view1/*');
 if($arrayFiles){
-  foreach ($arrayFiles as $filename) { ?>
-<table> <tr><td>
-<img src="<?php echo $filename; ?>" />
-</td></tr>
+//sort($arrayFiles);
+//  foreach ($arrayFiles as $filename) { ?>
+<table> 
+<tr><td><img src="img/view1/hour.png" /></td></tr>
+<tr><td><img src="img/view1/day.png" /></td></tr>
+<tr><td><img src="img/view1/week.png" /></td></tr>
+<tr><td><img src="img/view1/month.png" /></td></tr>
+<tr><td><img src="img/view1/year.png" /></td></tr>
+</table>
+
 <?php
- }} else { ?>
+// }
+} else { ?>
  <span class="brak">
 <img src="media/ico/Sign-Stop-icon.png" />
 </span>
 <?php } ?>
-</table>
+
 </span></span>	
