0cc514164c36918c70b46ec729574e2ed4c17f77
root <root@raspberrypi.(none)>
2014-05-18 07:47:16 +0200

    change licence to f4a

--- modules/info/info.php
+++ modules/info/info.php
@@ -1,8 +1,10 @@
 <div id="left"><span class="belka">&nbsp info<span class="okno">
 </br >
-<center><a href="http://nettemp.pl" target="_blank">www.nettemp.pl</a></center>
+<center><a href="http://nettemp.pl" target="_blank">nettemp.pl</a></center>
+<center><a href="http://techfreak.pl/nettemp" target="_blank">techfreak.pl/nettemp</a></center>
 </br >
-<center>techfreak.pl@gmail.com</center>
+<center>Support:</center>
+<center><a href="http://forum.techfreak.pl" target="_blank">forum.techfreak.pl</a></center>
 <br />
 <center>Credits:<br />
 RRDtool<br />
@@ -19,7 +21,7 @@ Gammu<br />
 <center><?php include('paypal.php'); ?></center>
 <br />
 
-<center><a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a></center>
+<center><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</center>
 
 
 
