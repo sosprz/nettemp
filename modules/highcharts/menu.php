59c31c784cacfeedcf70e4c8371845034b1b1737
root <root@raspberrypi>
2014-08-13 10:00:45 +0200

    add to highcharts more views week,month, year

---
+++ modules/highcharts/menu.php
@@ -0,0 +1,29 @@
+<span class="belka">&nbsp Highcharts<span class="okno">
+
+<table><tr>
+<td><a href="index.php?id=view&highcharts=day" ><button>day</button></a></td>
+<td><a href="index.php?id=view&highcharts=week" ><button>week</button></a></td>
+<td><a href="index.php?id=view&highcharts=month" ><button>month</button></a></td>
+<td><a href="index.php?id=view&highcharts=year" ><button>year</button></a></td>
+</tr>
+</table>
+
+
+
+<?php $art=$_GET['highcharts']; ?>
+<?php  
+switch ($art)
+{ 
+default: case '$art': include('modules/highcharts/highcharts.php'); break;
+case 'day': include('modules/highcharts/highcharts.php'); break;
+case 'week': include('modules/highcharts/highcharts_week.php'); break;
+case 'month': include('modules/highcharts/highcharts_month.php'); break;
+case 'year': include('modules/highcharts/highcharts_year.php'); 
+}
+?>
+
+
+
+
+</span>
+</span>
