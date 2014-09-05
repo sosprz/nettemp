831abcbfaa7a507de68a644203b08c44f7e54ba7
root <root@raspberrypi>
2014-09-05 23:08:02 +0200

    remove kwh status form gpio

--- modules/gpio/html/gpio_settings.php
+++ modules/gpio/html/gpio_settings.php
@@ -482,7 +482,7 @@ exec("$dir/gpio2 status $gpio", $out_arr);
    </form>
     
 	<form action="gpio" method="post">
-		<td>kWh <?php echo $a["gpio_kwh"]; ?></td>
+		<td>kWh</td>
     	<td><input type="image" src="media/ico/Lamp-icon.png" title="kWh metter" name="gpio_kwh" value="on" <?php echo $a["gpio_kwh"] == 'on' ? 'checked="checked"' : ''; ?> onclick="this.form.submit()" /></td>
     	<input type="hidden" name="gpio_kwh1" value="gpio_kwh2" />
 		<input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
