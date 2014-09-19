4526e5b44b3f421009b4f4ca81643d310542a117
root <root@raspberrypi>
2014-09-19 11:04:36 +0200

    add thanks to donators to info

--- modules/info/info.php
+++ modules/info/info.php
@@ -2,11 +2,12 @@
 </br >
 <center><a href="http://nettemp.pl" target="_blank">nettemp.pl</a></center>
 <center><a href="http://techfreak.pl/nettemp" target="_blank">techfreak.pl/nettemp</a></center>
-</br >
-<center>Support:</center>
+<center><h3>Tanks to</h3></center>
+<center>Pawel K, Robert G, Dariusz K, Dariusz P, Radoslaw S, Piotr P, Adrian D, Michal K, Tobias H, Robert A, Zbigniew S, Aleksy B, Dawid W</center>
+<center><h3>Support</h3></center>
 <center><a href="http://forum.techfreak.pl" target="_blank">forum.techfreak.pl</a></center>
-<br />
-<center>Credits:<br />
+<center><a href="mailto:techfreak.pL@gmail.com">techfreak.pl@gmail.com</a></center>
+<center><h3>Credits</h3>
 RRDtool<br />
 Adafriut<br />
 WiringPI<br />
@@ -14,16 +15,9 @@ Highcharts<br />
 Digitemp<br />
 Gammu<br />
 </center>
-
-<br />
-<center>Donate for developing:</center>
-<br />
+<center><h3>Donate for developing</h3></center>
 <center><?php include('paypal.php'); ?></center>
-<br />
-
+<center><h3>Licence</h3></center>
 <center><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.</center>
-
-
-
 </span></span>
 </div>
