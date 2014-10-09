73cdac60b3a3f279ccabf88af9368e080ed1de89
root <root@raspberrypi.(none)>
2014-10-09 11:16:36 +0000

    pre update_tools

--- modules/tools/html/tools_update.php
+++ modules/tools/html/tools_update.php
@@ -1,22 +1,22 @@
 <span class="belka">&nbsp Update<span class="okno">
 <?php
-$update=isset($_POST['update']) ? $_POST['update'] : '';
-if ($update == "Update") { 
-//putenv('PATH='. getenv('PATH') .':var/www/nettemp');
-
-exec("/usr/bin/git reset --hard");
-passthru("/usr/bin/git pull 2>&1");
-$dir=getcwd();
-exec('modules/reset/update_db');
-
-
-//header("location: " . $_SERVER['REQUEST_URI']);
-//	exit();	
-	}
-	?>
-	<form action="index.php?id=tools&type=update" method="post">
-	  <input type="hidden" name="update" value="Update">
-     <input  type="submit" value="Update"  />
-     </form>   
-
-     </span></span>
+    $update=isset($_POST['update']) ? $_POST['update'] : '';
+    if ($update == "Update") { 
+	//putenv('PATH='. getenv('PATH') .':var/www/nettemp');
+	exec("/usr/bin/git reset --hard");
+?>
+<pre>
+<?php
+    passthru("/usr/bin/git pull 2>&1");
+?>
+</pre>
+<?php
+    exec('modules/reset/update_db');
+    //header("location: " . $_SERVER['REQUEST_URI']);
+    //	exit();	
+} ?>
+    <form action="index.php?id=tools&type=update" method="post">
+    <input type="hidden" name="update" value="Update">
+    <input  type="submit" value="Update"  />
+    </form>
+    </span></span>
