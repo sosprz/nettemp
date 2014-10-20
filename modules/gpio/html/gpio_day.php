7b9d1d18b6f9f7918960f55c5f08606895068aa2
root <root@raspberrypi.(none)>
2014-10-20 06:50:43 +0000

    gpio

--- modules/gpio/html/gpio_day.php
+++ modules/gpio/html/gpio_day.php
@@ -11,10 +11,15 @@ if (($dayexit == "dayexit") ){
 
 $day_zone1s = isset($_POST['day_zone1s']) ? $_POST['day_zone1s'] : '';
 $day_zone1e = isset($_POST['day_zone1e']) ? $_POST['day_zone1e'] : '';
+$day_zone2s = isset($_POST['day_zone2s']) ? $_POST['day_zone2s'] : '';
+$day_zone2e = isset($_POST['day_zone2e']) ? $_POST['day_zone2e'] : '';
+$day_zone3s = isset($_POST['day_zone3s']) ? $_POST['day_zone3s'] : '';
+$day_zone3e = isset($_POST['day_zone3e']) ? $_POST['day_zone3e'] : '';
+
 $dayrun = isset($_POST['dayrun']) ? $_POST['dayrun'] : '';
 if ($dayrun == "on")  {
     $db = new PDO('sqlite:dbf/nettemp.db') or die("cannot open the database");
-    $db->exec("UPDATE gpio SET day_run='on', day_zone1s='$day_zone1s',day_zone1e='$day_zone1e'  WHERE gpio='$gpio_post'") or die("exec error");
+    $db->exec("UPDATE gpio SET day_run='on',day_zone1s='$day_zone1s',day_zone1e='$day_zone1e',day_zone2s='$day_zone2s',day_zone2e='$day_zone2e',day_zone3s='$day_zone3s',day_zone3e='$day_zone3e'  WHERE gpio='$gpio_post'") or die("exec error");
     $db = null;
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();	
@@ -38,7 +43,9 @@ $db = new PDO('sqlite:dbf/nettemp.db') or die("cannot open the database");
 ?>
 
     <form action="" method="post">
-	<td>Status: <?php echo $a['day_zone1s']; ?> <?php echo $a['day_zone1e']; ?> </td> 
+	<td>Status: <?php echo $a['day_zone1s']; ?>-<?php echo $a['day_zone1e']; ?> </td> 
+	<td><?php echo $a['day_zone2s']; ?>-<?php echo $a['day_zone2e']; ?> </td> 
+	<td><?php echo $a['day_zone3s']; ?>-<?php echo $a['day_zone3e']; ?> </td> 
 	<input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
 	<td><input type="image" src="media/ico/Button-Turn-Off-icon.png"/></td>
 	<input type="hidden" name="dayrun" value="off" />
@@ -59,14 +66,30 @@ include('gpio_rev.php');
    </form>
 
     <form action="" method="post">
-	<td>Start time</td>
+	<td>
+	<table>
+	<tr><td>Start time</td>
 	<td><input type="text" name="day_zone1s" value="<?php echo $a['day_zone1s']; ?>" size="8"  ></td><td></td> 
 	<td>End time</td>
 	<td><input type="text" name="day_zone1e" value="<?php echo $a['day_zone1e']; ?>" size="8"  ></td><td></td> 
+	</tr><tr>	
+	<td>Start time</td>
+	<td><input type="text" name="day_zone2s" value="<?php echo $a['day_zone2s']; ?>" size="8"  ></td><td></td> 
+	<td>End time</td>
+	<td><input type="text" name="day_zone2e" value="<?php echo $a['day_zone2e']; ?>" size="8"  ></td><td></td> 
+	</tr><tr>	
+	<td>Start time</td>
+	<td><input type="text" name="day_zone3s" value="<?php echo $a['day_zone3s']; ?>" size="8"  ></td><td></td> 
+	<td>End time</td>
+	<td><input type="text" name="day_zone3e" value="<?php echo $a['day_zone3e']; ?>" size="8"  ></td><td></td> 
+	</tr>
+	</table>
+	</td>
 	<input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
 	<td><input type="image" src="media/ico/Button-Turn-On-icon.png"/></td>
 	<input type="hidden" name="dayrun" value="on" />
-	<input type="hidden" name="on" value="on" />
+	
+	
     </form>
 <?php
     }}
--- modules/gpio/html/gpio_temp.php
+++ modules/gpio/html/gpio_temp.php
@@ -1,6 +1,6 @@
 <?php
 $tempexit = isset($_POST['tempexit']) ? $_POST['tempexit'] : '';
-if (($tempexit == "tempexit") ){
+if ($tempexit == "tempexit"){
     $db = new PDO('sqlite:dbf/nettemp.db') or die("cannot open the database");
     $db->exec("UPDATE gpio SET mode='' where gpio='$gpio_post' ") or die("simple off db error");
      $db = null;
@@ -8,63 +8,67 @@ if (($tempexit == "tempexit") ){
     exit();
     }
 
-
-
 // temp
 $temp_sensor = isset($_POST['temp_sensor']) ? $_POST['temp_sensor'] : '';
 $temp_onoff = isset($_POST['temp_onoff']) ? $_POST['temp_onoff'] : '';
 $temp_op = isset($_POST['temp_op']) ? $_POST['temp_op'] : '';
-
-//  $temp_grlo=$_POST['temp_grlo'];
-
 $temp_temp = isset($_POST['temp_temp']) ? $_POST['temp_temp'] : '';
+
+$temp_sensor1 = isset($_POST['temp_sensor1']) ? $_POST['temp_sensor1'] : '';
+$temp_onoff1 = isset($_POST['temp_onoff1']) ? $_POST['temp_onoff1'] : '';
+$temp_op1 = isset($_POST['temp_op1']) ? $_POST['temp_op1'] : '';
+$temp_temp1 = isset($_POST['temp_temp1']) ? $_POST['temp_temp1'] : '';
+
+$temp_sensor2 = isset($_POST['temp_sensor2']) ? $_POST['temp_sensor2'] : '';
+$temp_onoff2 = isset($_POST['temp_onoff2']) ? $_POST['temp_onoff2'] : '';
+$temp_op2 = isset($_POST['temp_op2']) ? $_POST['temp_op2'] : '';
+$temp_temp2 = isset($_POST['temp_temp2']) ? $_POST['temp_temp2'] : '';
+
+
+
 $tempon = isset($_POST['tempon']) ? $_POST['tempon'] : '';
-if ($tempon == "tempON")  {
+if ($tempon == "on") {
     $db = new PDO('sqlite:dbf/nettemp.db') or die("cannot open the database");
-    $db->exec("UPDATE gpio SET temp_run='on',temp_op='$temp_op',temp_sensor='$temp_sensor',temp_onoff='$temp_onoff',temp_temp='$temp_temp' WHERE gpio='$gpio_post'") or die("exec error");
-    if (!empty($day_zone1s) && !empty($day_zone1e)) {
-	$db->exec("UPDATE gpio SET tempday_run='on',day_zone1s='$day_zone1s',day_zone1e='$day_zone1e' WHERE gpio='$gpio_post'") or die("exec error");
-	}
+    $db->exec("UPDATE gpio SET temp_op='$temp_op',temp_sensor='$temp_sensor',temp_onoff='$temp_onoff',temp_temp='$temp_temp' WHERE gpio='$gpio_post'") or die("exec error");
+    $db->exec("UPDATE gpio SET temp_op1='$temp_op1',temp_sensor1='$temp_sensor1',temp_onoff1='$temp_onoff1',temp_temp1='$temp_temp1' WHERE gpio='$gpio_post'") or die("exec error");
+    $db->exec("UPDATE gpio SET temp_op2='$temp_op2',temp_sensor2='$temp_sensor2',temp_onoff2='$temp_onoff2',temp_temp2='$temp_temp2' WHERE gpio='$gpio_post'") or die("exec error");
     $db = null;
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();	
     }
 
-$day_zone1s = isset($_POST['day_zone1s']) ? $_POST['day_zone1s'] : '';
-$day_zone1e = isset($_POST['day_zone1e']) ? $_POST['day_zone1e'] : '';
-$tempdayon = isset($_POST['tempdayon']) ? $_POST['tempdayon'] : '';
-if ($tempdayon == "on")  {
+$dayrunon = isset($_POST['dayrunon']) ? $_POST['dayrunon'] : '';
+$dayrun = isset($_POST['dayrun']) ? $_POST['dayrun'] : '';
+if ($dayrunon == "on")  {
     $db = new PDO('sqlite:dbf/nettemp.db') or die("cannot open the database");
-    $db->exec("UPDATE gpio SET day_run='on', day_zone1s='$day_zone1s',day_zone1e='$day_zone1e'  WHERE gpio='$gpio_post'") or die("exec error");
-    $db = null;
-    header("location: " . $_SERVER['REQUEST_URI']);
-    exit();	
-    }
-    else {
-    $db = new PDO('sqlite:dbf/nettemp.db') or die("cannot open the database");
-    $db->exec("UPDATE gpio SET day_run='' WHERE gpio='$gpio_post'") or die("exec error");
+    $db->exec("UPDATE gpio SET day_run='$dayrun'  WHERE gpio='$gpio_post'") or die("exec error");
     $db = null;
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();	
     }
 ?>
 
-    <form action="" method="post">
-	<td><input type="image" name="tempoff" value="off" src="media/ico/Close-2-icon.png" title="Back"  onclick="this.form.submit()" /><td>
-	<input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
-	<input type="hidden" name="tempexit" value="tempexit" />
-	</form>
-    
-    <td>if</td>
-      <td>
-    <form action="" method="post">
+<form action="" method="post">
+    <td><input type="image" name="tempexit" value="tempexit" src="media/ico/Close-2-icon.png" title="Back"  onclick="this.form.submit()" /><td>
+    <input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
+</form>
+<form action="" method="post">
+	    <td><img  src="media/ico/day-icon.png" title="Day plan" /></td>
+	    <td><input type="checkbox" name="dayrun" value="on" <?php echo $a["day_run"] == 'on' ? 'checked="checked"' : ''; ?>  onclick="this.form.submit()" /><td>
+	    <input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
+	    <input type="hidden" name="dayrunon" value="on" /> 
+</form>
+
+<td>if</td>
+<td>
+<form action="" method="post">
 	<select name="temp_sensor" >
-	<?php $sth = $db->prepare("SELECT * FROM sensors");
-	$sth->execute();
-	$result = $sth->fetchAll();
-	foreach ($result as $select) { ?>
-	<option <?php echo $a['temp_sensor'] == $select['id'] ? 'selected="selected"' : ''; ?> value="<?php echo $select['id']; ?>"><?php echo "{$select['name']}  {$select['tmp']}" ?>&deg;C</option>
-	<?php } ?>
+	    <?php $sth = $db->prepare("SELECT * FROM sensors");
+	    $sth->execute();
+	    $result = $sth->fetchAll();
+	    foreach ($result as $select) { ?>
+		<option <?php echo $a['temp_sensor'] == $select['id'] ? 'selected="selected"' : ''; ?> value="<?php echo $select['id']; ?>"><?php echo "{$select['name']}  {$select['tmp']}" ?>&deg;C</option>
+	    <?php } ?>
         </select></td>
 	<td>
     <select name="temp_op" >
@@ -81,20 +85,11 @@ if ($tempdayon == "on")  {
         <option <?php echo $a['temp_onoff'] == 'on' ? 'selected="selected"' : ''; ?> value="on">On</option>   
         <option <?php echo $a['temp_onoff'] == 'off' ? 'selected="selected"' : ''; ?> value="off">Off</option>     
         </select></td>
-	<form action="" method="post">
-	    <td><img  src="media/ico/day-icon.png" title="Day plan" /></td>
-	    <td><input type="checkbox" name="tempday" value="on" <?php echo $a["day_run"] == 'on' ? 'checked="checked"' : ''; ?>  onclick="this.form.submit()" /><td>
-	    <input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
-	    <input type="hidden" name="tempdayon" value="on" />
-	</form>
-	<?php if ($a['day_run'] == 'on') { ?>
-        <td>Time</td>
-        <td><input type="text" name="day_zone1s" value="<?php echo $a['day_zone1s']; ?>" size="4"  ></td><td></td> 
-        <td>to</td>
-        <td><input type="text" name="day_zone1e" value="<?php echo $a['day_zone1e']; ?>" size="4"  ></td><td></td> 
-	<?php } ?>
-	<input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
-	<td><input type="image" src="media/ico/Button-Turn-On-icon.png"/></td>
-	<input type="hidden" name="tempon" value="on" />
 	
+<input type="hidden" name="gpio" value="<?php echo $a['gpio']; ?>"/>
+<td><input type="image" src="media/ico/Button-Turn-On-icon.png"/></td>
+<input type="hidden" name="tempon" value="on" />
 </form>
+
+
+
