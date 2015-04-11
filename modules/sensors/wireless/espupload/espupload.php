a4d1a53c730e140a716e292576f6b89b8e90f510
root <root@raspberrypi.(none)>
2015-04-11 22:58:45 +0200

    add wifi settings to ESP upload

--- modules/sensors/wireless/espupload/espupload.php
+++ modules/sensors/wireless/espupload/espupload.php
@@ -9,28 +9,50 @@ $ssid = $_POST["ssid"];
 $pass = $_POST["pass"];
 if ($_POST['run'] == "run") {
 
-    if (!empty($usb)) {
+    if (empty($usb)) {
 	if (!empty($ds18b20) || !empty($dht11)) {
 	    if (!empty($ssid) && !empty($pass)) {
 		    if (!empty($dht11) ) {
-			$cmd="'$dir'/modules/sensors/wireless/espupload/luatool.py -p '$usb' -f '$dir'/modules/sensors/wireless/DHT11/init.lua -t init.lua 2>&1";
-			$cmd2="'$dir'/modules/sensors/wireless/espupload/luatool.py -p '$usb' -f '$dir'/modules/sensors/wireless/DHT11/dht.lua -t dht.lua 2>&1";
+
+			$cmd0="cp '$dir'/modules/sensors/wireless/DHT11/init.lua '$dir'/tmp && sed -i s/pass/'$pass'/g '$dir'/tmp/init.lua && sed -i s/ssid/'$ssid'/g '$dir'/tmp/init.lua";
+			$cmd1="'$dir'/modules/sensors/wireless/espupload/luatool.py -p '$usb' -f '$dir'/tmp/init.lua -t init.lua 2>&1";
+			$cmd2="'$dir'/modules/sensors/wireless/espupload/luatool.py -r -p '$usb' -f '$dir'/modules/sensors/wireless/DHT11/dht.lua -t dht.lua 2>&1";
 			echo '<pre>';
-			passthru($cmd); 
-			passthru($cmd2); 
+			passthru($cmd0); 
+			passthru($cmd1); 
+			passthru($cmd2);
 			echo '</pre>';
 		    }
 		    if (!empty($ds18b20) ) {
-			$cmd="'$dir'/modules/sensors/wireless/espupload/luatool.py -r -p '$usb' -f '$dir'/modules/sensors/wireless/dh18b20/init.lua -t init.lua 2>&1";
+			$cmd0="cp '$dir'/modules/sensors/wireless/ds18b20/init.lua '$dir'/tmp && sed -i s/pass/'$pass'/g '$dir'/tmp/init.lua && sed -i s/ssid/'$ssid'/g '$dir'/tmp/init.lua";
+			$cmd1="'$dir'/modules/sensors/wireless/espupload/luatool.py -p '$usb' -f '$dir'/tmp/init.lua -t init.lua 2>&1";
 			echo '<pre>';
-			passthru($cmd); 
+			passthru($cmd0); 
+			passthru($cmd1); 
 			echo '</pre>';
 		    }
-			$cmd="echo 'wifi.sta.config('\"'$ssid'\"','\"'$pass'\"');' > '$usb'";
-			//echo "wifi.sta.config(\"gateway7\",\"142w1f1m0rm0w1f1142\")" > /dev/ttyUSB0
-	    		$cmd2="echo  'node.restart();' > '$usb'";
-			passthru($cmd); 
-			passthru($cmd2); 
+		    
+			//file.open("init.lua", "a+")
+		        //file.writeline('foo bar')
+			//file.close()
+			//$cmd1="echo 'wifi.setmode(wifi.STATION)' > '$usb'";
+			//$cmd2="echo 'wifi.sta.config('\"'$ssid'\"','\"'$pass'\"');' > '$usb'";
+
+			//echo "wifi.setmode(wifi.STATION)" > /dev/ttyUSB0
+			//echo "wifi.sta.config(\"ap\",\"pass\")" > /dev/ttyUSB0
+			
+
+			//$cmd1="echo 'file.open('\"'init.lua'\"', '\"'a+'\"')' > '$usb'";
+			//$cmd2="echo 'file.writeline('wifi.setmode(wifi.STATION)')' > '$usb'";
+			//cmd3="echo 'file.writeline('wifi.sta.config('\"'$ssid'\"','\"'$pass'\"')')' > '$usb'";
+			//$cmd4="echo 'file.close()' > '$usb'";
+			//$cmd1="echo 'wifi.setmode(wifi.STATION)' > '$usb'";
+			//$cmd2="echo 'wifi.sta.config('\"'$ssid'\"','\"'$pass'\"');' > '$usb'";
+	    		//$cmd3="echo  'node.restart();' > '$usb'";
+			//passthru($cmd1); 
+			//passthru($cmd2); 
+			//passthru($cmd3); 
+			//passthru($cmd4); 
 	    }
 	    else {
 		    echo "Error - no SSID and Password"; 
