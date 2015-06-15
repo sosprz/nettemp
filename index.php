a28bdc9e551fd5a1581b0fd9a0653e0109a307a2
root <root@raspberrypi.(none)>
2015-06-15 08:02:30 +0000

    start

--- index.php
+++ index.php
@@ -13,33 +13,78 @@ if ( '' == file_get_contents( $dbfile ) )
 <?php }
 else {
 ?>
-<?php session_start(); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html>
-<head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<link rel="stylesheet" href="media/style.css" type="text/css"/>
-<link href="media/menu.css" rel="stylesheet" type="text/css">
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="media/custom.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+ <!-- Static navbar -->
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+		<img src="media/png/nettemp.pl.png" lass="img-rounded">
+
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+	              </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li <?php echo $id == 'status' ? ' class="active"' : ''; ?>><a href="status" >Status</a></li>
+              <li <?php echo $id == 'view' ? ' class="active"' : ''; ?>><a href="view" >Charts </a></li>
+              <li <?php echo $id == 'info' ? ' class="active"' : ''; ?>><a href="info">Info</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Settings<span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+	    <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Email" class="form-control">
+            </div>
+            <div class="form-group">
+              <input type="password" placeholder="Password" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Sign in</button>
+          </form>        
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+
+<div class="container">
 
 
-
-<script type="text/JavaScript">
-<!-- function timedRefresh(timeoutPeriod) {
-    setTimeout("location.reload(true);",timeoutPeriod);
-    }   -->
-</script>
-</head>
-<!-- <body onload="JavaScript:timedRefresh(6000);"> -->
-
-<div id="top">
-<div id="header">
-<span class="logo"><a href="http://nettemp.pl" target="_blank"><img src="media/png/nettemp.pl.png" alt="Monitoring Temperatury"></a></span>
-<span class="login"><?php include("modules/login/login.php"); ?></span>
-</div>
-<div id="tabs22">
-<ul> 
-    <li><a href='status' <?php echo $id == 'status' ? ' class="active"' : ''; ?> ><span <?php echo $id == 'status' ? ' class="active"' : ''; ?> >Status</span></a></li>
-	<li><a href='view' <?php echo $id == 'view' ? ' class="active"' : ''; ?> ><span <?php echo $id == 'view' ? ' class="active"' : ''; ?> >Charts</span></a></li>
 	<?php if ($numRows1 == 1 && ( $perms == "adm" )) { ?>
 		<li><a href='devices'<?php echo $id == 'devices' ? ' class="active"' : ''; ?>><span <?php echo $id == 'devices' ? ' class="active"' : ''; ?>>Devices</span></a></li>
 		<li><a href='notification'<?php echo $id == 'notification' ? ' class="active"' : ''; ?>><span <?php echo $id == 'notification' ? ' class="active"' : ''; ?>>Notification</span></a></li>
@@ -48,13 +93,7 @@ else {
 		<li><a href='tools'<?php echo $id == 'tools' ? ' class="active"' : ''; ?>><span <?php echo $id == 'tools' ? ' class="active"' : ''; ?>>Tools</span></a></li> 
 		<li><a href='info'<?php echo $id == 'info' ? ' class="active"' : ''; ?>><span <?php echo $id == 'info' ? ' class="active"' : ''; ?>>Info</span></a></li>
  <?php } 
-else {?>
-		<li><a href='info'<?php echo $id == 'info' ? ' class="active"' : ''; ?> ><span <?php echo $id == 'info' ? ' class="active"' : ''; ?>>Info</span></a></li>
-<?php
-} ?>
-</ul>
-</div>
-<div id="center">
+?>
 <?php  
 switch ($id)
 { 
@@ -75,17 +114,30 @@ case 'espupload': include('modules/sensors/wireless/espupload/espupload.php'); b
 }
 ?>
 
-</div>
-
-	<div id="footer"><center><table><tr><td>Donate for developing</td><td> <?php include('modules/info/paypal.php'); ?></td><td>v <?php passthru("awk '/Changelog/{y=1;next}y' readme.md |head -2 |grep -v '^$' && /usr/bin/git branch |grep [*]|awk '{print $2}';"); ?></td><td>| System time <?php $today=date("H:i:s"); echo $today;?></td></tr></table></center>
-</div>
-</div>
-
-</body>
-</html>
 <?php } 
-ob_end_flush();
 ?>
+
+
+
+
+</div>
+
+
+
+
+<footer class="footer">
+      <div class="container text-center">
+        <p class="text-muted">Donate for developing paypal <?php //include('modules/info/paypal.php'); ?> build <?php passthru("/usr/bin/git branch |grep [*]|awk '{print $2}' && awk '/Changelog/{y=1;next}y' readme.md |head -2 |grep -v '^$'"); ?></td><td>| System time <?php $today=date("H:i:s"); echo $today;?></p>
+      </div>
+    </footer>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
 
 
 
--- modules/sensors/html/sensor_status.php
+++ modules/sensors/html/sensor_status.php
@@ -17,134 +17,127 @@ if ($numRows == 0 ) { echo "<span class=\"empty\"><img src=\"media/ico/Sign-Stop
 	if($a['type'] == 'temp'){ 
 		
 		if($a['tmp'] >= $a['tmp_max'] && !empty($a['tmp']) && !empty($a['tmp_max']) && $a['alarm'] == on ) { ?> 
-		<tr>
-		    <td><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></td>
-		    <td><img src="media/ico/temp_high.png" /></td>
-		    <td><font color="#FF0000"> <?php echo $name ?></font></td>
-		    <td><font color="#FF0000"><?php echo $a['tmp']; echo " C "; echo "max:"; echo $a['tmp_max'];?> &#176;C</font></td>
-		</tr>
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/temp_high.png" /></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"> <?php echo $name ?></font></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"><?php echo $a['tmp']; echo " C "; echo "max:"; echo $a['tmp_max'];?> &#176;C</font></div>
+		</div>
 		<?php	}
 		elseif($a['tmp'] <= $a['tmp_min'] && !empty($a['tmp']) && !empty($a['tmp_min']) && $a['alarm'] == on ) { ?> 
-		<tr>
-		    <td><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></td>
-		    <td><img src="media/ico/temp_low.png" /></td>
-		    <td><font color="#0095FF"> <?php echo $name ?></td>
-		    <td><font color="#0095FF"><?php echo $a['tmp']; echo " C "; echo "min:"; echo $a['tmp_min'];?> &#176;C</font></td>
-		</tr>
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/temp_low.png" /></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"> <?php echo $name ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"><?php echo $a['tmp']; echo " C "; echo "min:"; echo $a['tmp_min'];?> &#176;C</font></div>
+		</div>
 		<?php	}
 		else { ?> 
-		<tr>
-		    <td><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></td>
-		    <td><img src="media/ico/temp2-icon.png" /></td>
-		    <td><font  color="#108218"> <?php echo $name ?> </td>
-		    <td><font  color="#108218"> <?php echo $a['tmp'];?> &#176;C</td>
-		    <td><?php if($a['tmp'] > $a['tmp_5ago']) { ?><img src="media/ico/Up-3-icon.png" /><?php } elseif($a['tmp'] < $a['tmp_5ago']){?><img src="media/ico/Down-3-icon.png" /><?php } ?></td>
-		</tr></font> 
+		<div class="row">
+		    <div class="col-xs-1 col-sm-1 col-lg-1"><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></div>
+		    <div class="col-xs-1 col-sm-1 col-lg-1"><img src="media/ico/temp2-icon.png" /></div>
+		    <div class="col-xs-5 col-sm-4 col-lg-5"><font  color="#108218"> <?php echo $name ?> </div>
+		    <div class="col-xs-3 col-sm-3 col-lg-3"><font  color="#108218"> <?php echo $a['tmp'];?> &#176;C</div>
+		    <div class="col-xs-1 col-sm-1 col-lg-1"><?php if($a['tmp'] > $a['tmp_5ago']) { ?><img src="media/ico/Up-3-icon.png" /><?php } elseif($a['tmp'] < $a['tmp_5ago']){?><img src="media/ico/Down-3-icon.png" /><?php } ?></div>
+		</div></font> 
 		<?php }
 	}
-	if($a['type'] == 'snmp'){
+	if($a['type'] == '1snmp'){
 		if($a['tmp'] >= $a['tmp_max'] && !empty($a['tmp']) && !empty($a['tmp_max']) && $a['alarm'] == on ) { ?> 
-		<tr>
-		    <td></td>
-		    <td><img src="media/ico/temp_high.png" /></td>
-		    <td><font color="#FF0000"> <?php echo $name ?></td>
-		    <td><font color="#FF0000"><?php echo $a['tmp'];echo " C "; echo "max:"; $a['tmp_max'];?> &#176;C</font></td>
-		</tr>
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/temp_high.png" /></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"> <?php echo $name ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"><?php echo $a['tmp'];echo " C "; echo "max:"; $a['tmp_max'];?> &#176;C</font></div>
+		</div>
 		<?php	}
 		elseif($a['tmp'] <= $a['tmp_min'] && !empty($a['tmp']) && !empty($a['tmp_min']) && $a['alarm'] == on ) { ?> 
-		<tr>
-		    <td></td>
-		    <td><img src="media/ico/temp_low.png" /></td><td><font color="#0095FF"> <?php echo $name ?></td>
-		    <td><font color="#0095FF"><?php echo $a['tmp']; echo " C "; echo "min:"; $a['tmp_min'];?> &#176;C</font></td>
-		</tr>
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/temp_low.png" /></div><div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"> <?php echo $name ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"><?php echo $a['tmp']; echo " C "; echo "min:"; $a['tmp_min'];?> &#176;C</font></div>
+		</div>
 		<?php	}
 		else { ?> 
-		<tr>
-		    <td></td>
-		    <td><img src="media/ico/temp2-icon.png" /></td>
-		    <td><font  color="#108218"> <?php echo $name ?> </td>
-		    <td><font  color="#108218"> <?php echo $a['tmp'];?> &#176;C</font></td>
-		    <td><?php if($a['tmp'] > $a['tmp_5ago']) { ?><img src="media/ico/Up-3-icon.png" /><?php } elseif($a['tmp'] < $a['tmp_5ago']){?><img src="media/ico/Down-3-icon.png" /><?php } ?></td>
-		</tr> 
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/temp2-icon.png" /></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font  color="#108218"> <?php echo $name ?> </div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font  color="#108218"> <?php echo $a['tmp'];?> &#176;C</font></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><?php if($a['tmp'] > $a['tmp_5ago']) { ?><img src="media/ico/Up-3-icon.png" /><?php } elseif($a['tmp'] < $a['tmp_5ago']){?><img src="media/ico/Down-3-icon.png" /><?php } ?></div>
+		</div> 
 		<?php }
 	}
 	if($a['type'] == 'humid'){
 		if($a['tmp'] >= $a['tmp_max'] && !empty($a['tmp']) && !empty($a['tmp_max']) && $a['alarm'] == on ) { ?>
-		<tr>
-		    <td><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></td>
-		    <td><img src="media/ico/rain-icon.png" /></td><td><font color="#FF0000"> <?php echo $name ?></td>
-		    <td><font color="#FF0000"><?php echo $a['tmp']; echo " % "; echo "max:"; echo $a['tmp_max'];?>&nbsp%</font></td>
-		</tr>
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/rain-icon.png" /></div><div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"> <?php echo $name ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"><?php echo $a['tmp']; echo " % "; echo "max:"; echo $a['tmp_max'];?>&nbsp%</font></div>
+		</div>
 		<?php	}
 		elseif($a['tmp'] <= $a['tmp_min'] && !empty($a['tmp']) && !empty($a['tmp_min']) && $a['alarm'] == on ) { ?>
-		<tr>
-		    <td><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></td>
-		    <td><img src="media/ico/rain-icon.png" /></td><td><font color="#0095FF"> <?php echo $name ?></td>
-		    <td><font color="#0095FF"><?php echo $a['tmp']; echo " % "; echo "min:"; echo $a['tmp_min'];?>&nbsp%</font></td>
-		</tr>  
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/rain-icon.png" /></div><div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"> <?php echo $name ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"><?php echo $a['tmp']; echo " % "; echo "min:"; echo $a['tmp_min'];?>&nbsp%</font></div>
+		</div>  
 		<?php	}
-		else { ?> <tr>
-		    <td><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></td>
-		    <td><img src="media/ico/rain-icon.png" /></td><td><font  color="#108218"> <?php echo $name ?> </td>
-		    <td><font  color="#108218"> <?php echo $a['tmp'];?>&nbsp %</td>
-		    <td><?php if($a['tmp'] > $a['tmp_5ago']) { ?><img src="media/ico/Up-3-icon.png" /><?php } elseif($a['tmp'] < $a['tmp_5ago']){?><img src="media/ico/Down-3-icon.png" /><?php } ?></td>
-		</tr></font> 
+		else { ?> <div class="row">
+		    <div class="col-xs-1 col-sm-1 col-lg-1"><?php if($a['device'] == 'wireless'){ ?><img src="media/ico/wifi-circle-icon.png" /><?php } ?></div>
+		    <div class="col-xs-1 col-sm-2 col-lg-1"><img src="media/ico/rain-icon.png" /></div>
+		    <div class="col-xs-5 col-sm-4 col-lg-5"><font  color="#108218"> <?php echo $name ?> </div>
+		    <div class="col-xs-3 col-sm-2 col-lg-3"><font  color="#108218"> <?php echo $a['tmp'];?>&nbsp %</div>
+		    <div class="col-xs-1 col-sm-2 col-lg-1"><?php if($a['tmp'] > $a['tmp_5ago']) { ?><img src="media/ico/Up-3-icon.png" /><?php } elseif($a['tmp'] < $a['tmp_5ago']){?><img src="media/ico/Down-3-icon.png" /><?php } ?></div>
+		</div></font> 
 		<?php }
 	}
 	if($a['type'] == 'press'){
 		if($a['tmp'] >= $a['tmp_max'] && !empty($a['tmp']) && !empty($a['tmp_max']) && $a['alarm'] == on ) { ?> 
-		<tr>
-		    <td></td>
-		    <td><img src="media/ico/Science-Pressure-icon.png" /></td>
-		    <td><font color="#FF0000"> <?php echo $name ?></font></td>
-		    <td><font color="#FF0000"><?php echo $a['tmp']; echo " Pa "; echo "max:"; echo $a['tmp_max'];?>&nbspPa</font></td>
-		</tr>
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/Science-Pressure-icon.png" /></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"> <?php echo $name ?></font></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"><?php echo $a['tmp']; echo " Pa "; echo "max:"; echo $a['tmp_max'];?>&nbspPa</font></div>
+		</div>
 		<?php	}
 		elseif($a['tmp'] <= $a['tmp_min'] && !empty($a['tmp']) && !empty($a['tmp_min']) && $a['alarm'] == on ) { ?> 
-		<tr>
-		    <td></td>
-		    <td><img src="media/ico/Science-Pressure-icon.png" /></td>
-		    <td><font color="#0095FF"> <?php echo $name ?></font></td>
-		    <td><font color="#0095FF"><?php echo $a['tmp']; echo " Pa "; echo "min:"; echo $a['tmp_min'];?>&nbspPa</font></td>
-		</tr>  <?php	}
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/Science-Pressure-icon.png" /></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"> <?php echo $name ?></font></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"><?php echo $a['tmp']; echo " Pa "; echo "min:"; echo $a['tmp_min'];?>&nbspPa</font></div>
+		</div>  <?php	}
 		else { ?> 
-		<tr>
-		    <td></td>
-		    <td><img src="media/ico/Science-Pressure-icon.png" /></td>
-		    <td><font  color="#108218"> <?php echo $name?></font> </td>
-		    <td><font  color="#108218"> <?php echo $a['tmp'];?>&nbspPa</font></td>
-		    <td><?php if($a['tmp'] > $a['tmp_5ago']) { ?><img src="media/ico/Up-3-icon.png" /><?php } elseif($a['tmp'] < $a['tmp_5ago']){?><img src="media/ico/Down-3-icon.png" /><?php } ?></td>
-		</tr> 
+		<div class="row">
+		    <div class="col-xs-1 col-sm-1 col-lg-1"></div>
+		    <div class="col-xs-1 col-sm-1 col-lg-1"><img src="media/ico/Science-Pressure-icon.png" /></div>
+		    <div class="col-xs-5 col-sm-5 col-lg-5"><font  color="#108218"> <?php echo $name?></font> </div>
+		    <div class="col-xs-3 col-sm-3 col-lg-3"><font  color="#108218"> <?php echo $a['tmp'];?>&nbspPa</font></div>
+		    <div class="col-xs-1 col-sm-1 col-lg-1"><?php if($a['tmp'] > $a['tmp_5ago']) { ?><img src="media/ico/Up-3-icon.png" /><?php } elseif($a['tmp'] < $a['tmp_5ago']){?><img src="media/ico/Down-3-icon.png" /><?php } ?></div>
+		</div> 
 		<?php }
 	}
 	if($a['type'] == 'altitude'){
-		if($a['tmp'] >= $a['tmp_max'] && !empty($a['tmp']) && !empty($a['tmp_max']) && $a['alarm'] == on ) { ?> <tr><td></td><td><img src="media/ico/paper-plane-icon.png" /></td><td><font color="#FF0000"> <?php echo $name ?></td><td><font color="#FF0000"><?php echo $a['tmp']; echo " m "; "max:"; echo $a['tmp_max'];?>&nbspm</font></td></tr><?php	}
-		elseif($a['tmp'] <= $a['tmp_min'] && !empty($a['tmp']) && !empty($a['tmp_min']) && $a['alarm'] == on ) { ?> <tr><td></td><td><img src="media/ico/paper-plane-icon.png" /></td><td><font color="#0095FF"> <?php echo $name ?></td><td><font color="#0095FF"><?php echo $a['tmp']; echo " m "; echo "min:"; echo $a['tmp_min'];?>&nbspm</font></td></tr>  <?php	}
-		else { ?> <tr><td><img src="media/ico/paper-plane-icon.png" /></td><td><font  color="#108218"> <?php echo $name ?> </td><td><font  color="#108218"> <?php echo $a['tmp'];?>&nbspm</td></tr></font> <?php }
+		if($a['tmp'] >= $a['tmp_max'] && !empty($a['tmp']) && !empty($a['tmp_max']) && $a['alarm'] == on ) { ?> <div class="row"><div class="col-xs-2 col-sm-2 col-lg-2"></div><div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/paper-plane-icon.png" /></div><div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"> <?php echo $name ?></div><div class="col-xs-2 col-sm-2 col-lg-2"><font color="#FF0000"><?php echo $a['tmp']; echo " m "; "max:"; echo $a['tmp_max'];?>&nbspm</font></div></div><?php	}
+		elseif($a['tmp'] <= $a['tmp_min'] && !empty($a['tmp']) && !empty($a['tmp_min']) && $a['alarm'] == on ) { ?> <div class="row"><div class="col-xs-2 col-sm-2 col-lg-2"></div><div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/paper-plane-icon.png" /></div><div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"> <?php echo $name ?></div><div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"><?php echo $a['tmp']; echo " m "; echo "min:"; echo $a['tmp_min'];?>&nbspm</font></div></div>  <?php	}
+		else { ?> <div class="row"><div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/paper-plane-icon.png" /></div><div class="col-xs-2 col-sm-2 col-lg-2"><font  color="#108218"> <?php echo $name ?> </div><div class="col-xs-2 col-sm-2 col-lg-2"><font  color="#108218"> <?php echo $a['tmp'];?>&nbspm</div></div></font> <?php }
 	}
 	if($a['type'] == 'lux'){
 		if($a['tmp'] >= $a['tmp_max'] && !empty($a['tmp']) && !empty($a['tmp_max']) && $a['alarm'] == on ) { ?> 
-		<tr>
-		    <td></td>
-		    <td><img src="media/ico/sun-icon.png" /></td>
-		    <td><font color="#FF0000"> <?php echo $name ?></td>
-		    <td><font color="#FF0000"><?php echo $a['tmp']; echo " lux "; echo "max:"; echo $a['tmp_max'];?>&nbsplux</font></td>
-		</tr>
+		<div class="row">
+		    <div class="col-xs-1 col-sm-2 col-lg-2"><img src="media/ico/sun-icon.png" /></div>
+		    <div class="col-xs-5 col-sm-2 col-lg-2"><font color="#FF0000"> <?php echo $name ?></div>
+		    <div class="col-xs-3 col-sm-2 col-lg-2"><font color="#FF0000"><?php echo $a['tmp']; echo " lux "; echo "max:"; echo $a['tmp_max'];?>&nbsplux</font></div>
+		</div>
 		<?php	}
 		elseif($a['tmp'] <= $a['tmp_min'] && !empty($a['tmp']) && !empty($a['tmp_min']) && $a['alarm'] == on ) { ?> 
-		<tr>
-		    <td></td>
-		    <td><img src="media/ico/sun-icon.png" /></td>
-		    <td><font color="#0095FF"> <?php echo $name ?></td>
-		    <td><font color="#0095FF"><?php echo $a['tmp']; echo " lux "; echo "min:"; echo $a['tmp_min'];?>&nbsplux</font></td>
-		</tr>  <?php	}
+		<div class="row">
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><img src="media/ico/sun-icon.png" /></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"> <?php echo $name ?></div>
+		    <div class="col-xs-2 col-sm-2 col-lg-2"><font color="#0095FF"><?php echo $a['tmp']; echo " lux "; echo "min:"; echo $a['tmp_min'];?>&nbsplux</font></div>
+		</div>  <?php	}
 		else { ?>  
-		    <tr>
-			<td></td>
-			<td><img src="media/ico/sun-icon.png" /></td>
-			<td><font  color="#108218"> <?php echo $name ?> </td>
-			<td><font  color="#108218"> <?php echo $a['tmp'];?>&nbsplux</td>
-		    </tr>
+		    <div class="row">
+			<div class="col-xs-1 col-sm-1 col-lg-1"><img src="media/ico/sun-icon.png" /></div>
+			<div class="col-xs-5 col-sm-5 col-lg-5"><font  color="#108218"> <?php echo $name ?> </div>
+			<div class="col-xs-3 col-sm-3 col-lg-3"><font  color="#108218"> <?php echo $a['tmp'];?>&nbsplux</div>
+		    </div>
 	    </font> <?php }
 	}
 	
