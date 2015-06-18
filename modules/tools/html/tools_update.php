480ceddbf422744a154f4ff27c2963aafb84afe5
root <root@raspberrypi.(none)>
2015-06-18 10:49:20 +0200

    unlock update

--- modules/tools/html/tools_update.php
+++ modules/tools/html/tools_update.php
@@ -7,12 +7,12 @@
 <?php
     $update=isset($_POST['update']) ? $_POST['update'] : '';
     if ($update == "Update") { 
-	//putenv('PATH='. getenv('PATH') .':var/www/nettemp');
-	//exec("/usr/bin/git reset --hard");
+	putenv('PATH='. getenv('PATH') .':var/www/nettemp');
+	exec("/usr/bin/git reset --hard");
 ?>
 <pre>
 <?php
-    //passthru("/usr/bin/git pull 2>&1");
+    passthru("/usr/bin/git pull 2>&1");
 ?>
 </pre>
 <?php
