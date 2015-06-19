d9f5822011f13f4185d7de46be34827c81ae12be
root <root@raspberrypi.(none)>
2015-06-19 21:01:42 +0200

    scan

--- modules/sensors/html/sensors_device.php
+++ modules/sensors/html/sensors_device.php
@@ -3,7 +3,7 @@
               <h3 class="panel-title">Discovered devices</h3>
             </div>
             <div class="panel-body">
-<table class="table table-striped">
+<table class="table">
 <?php
 $dir='';
 $db1 = new PDO('sqlite:dbf/nettemp.db');
@@ -12,12 +12,12 @@ $sth->execute();
 $result = $sth->fetchAll();
 $separator = "\r\n";
 foreach ($result as $a) { ?>
-	<tr><td>USB  </td> <td><?php echo $a['usb']; ?></td></tr>
-	<tr><td>1-wire </td><td><?php echo  $a['onewire']; ?></td></tr>
-	<tr><td>Serial </td><td><?php echo  $a['serial']; ?></td></tr>
-	<tr><td>I2C </td><td><?php echo  $a['i2c']; ?></td></tr>
-	<tr><td>lm-sensors </td><td><?php echo  $a['lmsensors']; ?></td></tr>
-	<tr><td>wireless </td><td><?php echo  $a['wireless']; ?></td></tr>
+	<tr <?php echo $a['usb'] != 'off' ? 'class="info"' : ''; ?>><td>USB  </td> <td><?php echo $a['usb']; ?></td></tr>
+	<tr <?php echo $a['onewire'] != 'off' ? 'class="info"' : ''; ?>><td>1-wire </td><td><?php echo  $a['onewire']; ?></td></tr>
+	<tr <?php echo $a['serial'] != 'off' ? 'class="info"' : ''; ?>><td>Serial </td><td><?php echo  $a['serial']; ?></td></tr>
+	<tr <?php echo $a['i2c'] != 'off' ? 'class="info"' : ''; ?>><td>I2C </td><td><?php echo  $a['i2c']; ?></td></tr>
+	<tr <?php echo $a['lmsensors'] != 'off' ? 'class="info"' : ''; ?>><td>lm-sensors </td><td><?php echo  $a['lmsensors']; ?></td></tr>
+	<tr <?php echo $a['wireless'] != 'off' ? 'class="info"' : ''; ?>><td>wireless </td><td><?php echo  $a['wireless']; ?></td></tr>
 <?php 
     }
 ?>
@@ -32,17 +32,19 @@ foreach ($result as $a) { ?>
         exit();
 	}
 ?>
-<pre>
+
 <?php
-    if (file_exists("/var/www/nettemp/tmp/scan")) {
+    if (file_exists("/var/www/nettemp/tmp/scan")) { ?>
+    <pre><?php
     $array = file("/var/www/nettemp/tmp/scan");
-    //$last = array_slice($filearray,-100);
     foreach($array as $f){
 	echo $f;
 	}
+    ?></pre>
+<?php
     }
 ?>
-</pre>
+
 <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
 <input type="submit" name="scan" value="Scan for new sensors" class="btn btn-primary"/>
 </form>
