b307082db4d23072cf9143e59248981ccb4446b3
root <root@raspberrypi.(none)>
2015-06-22 16:06:46 +0200

    pin4to9

--- modules/sensors/wireless/relay/init.lua
+++ modules/sensors/wireless/relay/init.lua
@@ -1,17 +1,17 @@
 wifi.setmode(wifi.STATION)
 wifi.sta.config("ssid","pass")
 
-pin4 = 4
-gpio.mode(pin4,gpio.OUTPUT)
+pin = 9
+gpio.mode(pin,gpio.OUTPUT)
 
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
     conn:on(
 	"receive",function(conn,payload)
 	print(payload)
-	if string.find(payload,"on") ~= nil then gpio.write(pin4,gpio.HIGH) end
-	if string.find(payload,"off") ~= nil then gpio.write(pin4,gpio.LOW) end
-	if string.find(payload,"status") ~= nil then conn:send('status ' ..gpio.read(pin4)) end 
+	if string.find(payload,"on") ~= nil then gpio.write(pin,gpio.HIGH) end
+	if string.find(payload,"off") ~= nil then gpio.write(pin,gpio.LOW) end
+	if string.find(payload,"status") ~= nil then conn:send('status ' ..gpio.read(pin)) end 
 	conn:send("nettemp.pl relay") end
 	) 
     conn:on("sent",function(conn) conn:close() end) 
