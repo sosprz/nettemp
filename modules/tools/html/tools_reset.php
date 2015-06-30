4e581df84bc0a76e12e93c7fda82e073d0e9094f
root <root@raspberrypi.(none)>
2015-06-30 20:05:44 +0200

    info no db

--- modules/tools/html/tools_reset.php
+++ modules/tools/html/tools_reset.php
@@ -4,10 +4,10 @@
 </div>
 <div class="panel-body">
 <?php
+$dir=$_SERVER['DOCUMENT_ROOT'];
 $admin_db_reset = isset($_POST['admin_db_reset']) ? $_POST['admin_db_reset'] : '';
 if ($admin_db_reset == "admin_db_reset1") { 
-system ("cd modules/tools && sh db_reset");
-
+shell_exec("$dir/modules/tools/db_reset");
 header("location: " . $_SERVER['REQUEST_URI']);
 exit();	
 }
