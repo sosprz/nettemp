ac08dd460c6183c7b3407929e65b62a71e2f6acb
root <root@raspberrypi.(none)>
2015-07-13 23:35:45 +0200

    radius tables

--- modules/security/radius/html/certs.php
+++ modules/security/radius/html/certs.php
@@ -1,3 +1,6 @@
+<div class="panel panel-default">
+<div class="panel-heading"><h3 class="panel-title">Certifications</h3></div>
+
 <?php
 $rmu = isset($_POST['rmu']) ? $_POST['rmu'] : '';
 $rmuser = isset($_POST['rmuser']) ? $_POST['rmuser'] : '';
@@ -30,9 +33,9 @@ if ($add == "add"){
     <div class="form-group">
     <td ><input type="text" name="username" value="" class="form-control" required=""/></td>
     <td ><input type="text" name="mail" value="" class="form-control" required=""/></td>
-    <td ><input type="text" name="days" value="" class="form-control" /></td>
+    <td ><input type="text" name="days" value="" class="form-control" placeholder="ex. 15, default 365 "/></td>
     <input type="hidden" name="add" value="add" class="form-control"/>
-    <td><button class="btn btn-xs btn-success"></button></td>
+    <td><button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-plus"></span></button></td>
     </div>
     </form>
 </tr>
@@ -43,21 +46,34 @@ if ($add == "add"){
 $Mydir = '/usr/local/etc/raddb/certs/users/';
 foreach(glob($Mydir.'*', GLOB_ONLYDIR) as $dir) {
     $dir = str_replace($Mydir, '', $dir);
-    echo $dir;	
+    
     $cmd="sudo openssl x509 -in /usr/local/etc/raddb/certs/users/$dir/$dir.pem -text -noout |grep After| awk '{ print $4\" \"$5\" \"$6\" \"$7}'";
     $out=shell_exec($cmd);
     $cmd2="sudo openssl x509 -in /usr/local/etc/raddb/certs/users/$dir/$dir.pem  -text -noout |grep  'Subject.*CN'| awk -F\"=\" '{print $6}'";
     $out2=shell_exec($cmd2);
-    echo $out2;
-    echo $out;
-?>
-    <form action="" method="post" style=" display:inline!important;">
-        <input type="hidden" name="rmuser" value="<?php echo "$dir"; ?>" />
-        <input type="hidden" name="rmu" value="rmu" />
-	<button class="btn btn-xs btn-danger">Revoke</button>
-    </form>
+    ?>
+<tr>
+    <td>
+	<?php echo $dir; ?>
+    </td>
+    <td>
+	<?php echo $out2; ?>
+    </td>
+    <td>
+	<?php echo "expire: " . $out; ?>
+    </td>
+
+    <td>
+	<form action="" method="post" style=" display:inline!important;">
+    	    <input type="hidden" name="rmuser" value="<?php echo "$dir"; ?>" />
+    	    <input type="hidden" name="rmu" value="rmu" />
+	    <button class="btn btn-xs btn-danger">Revoke</button>
+	</form>
+    </td>
+</tr>
 <?php
 }
 ?>
-
-
+</table>
+</div>
+</div>
