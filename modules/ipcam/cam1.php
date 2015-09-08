b312a3307c59bb7364cf258c2ec153085cf6e7fb
root <root@raspberrypi.(none)>
2015-09-08 15:05:22 +0000

    ipcam - view if admin logged

--- modules/ipcam/cam1.php
+++ modules/ipcam/cam1.php
@@ -14,7 +14,7 @@ if ($numRows == 0 ) { return; }
 <div class="panel-body">
 <?php //if(!isset($_SESSION['user'])){ echo "Video available after login"; return; }
 ?>
-<embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org" autoplay="yes" loop="no" width="300" height="300" target="<?php if(!isset($_SESSION['user'])){ echo "Video available after login"; } else { echo $a['link'];} ?>" />
+<embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org" autoplay="yes" loop="no" width="300" height="300" target="<?php if($_SESSION['user'] != "admin"){ echo "Video available after login"; } else { echo $a['link'];} ?>" />
 <object classid="clsid:9BE31822-FDAD-461B-AD51-BE1D1C159921" codebase="http://download.videolan.org/pub/videolan/vlc/last/win32/axvlc.cab" style="display:none;"></object>
 </div>
 </div>
--- modules/ipcam/cam9.php
+++ modules/ipcam/cam9.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+
 $db = new PDO('sqlite:dbf/nettemp.db');
 $rows = $db->query("SELECT * FROM camera where id='9'");
 $row = $rows->fetchAll();
@@ -11,7 +12,9 @@ if ($numRows == 0 ) { return; }
 <h3 class="panel-title"><?php echo $a['name'] ?></h3>
 </div>
 <div class="panel-body">
-<embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org" autoplay="yes" loop="no" width="300" height="300" target="<?php echo $a['link'] ?>" />
+<?php //if(!isset($_SESSION['user'])){ echo "Video available after login"; return; }
+?>
+<embed type="application/x-vlc-plugin" pluginspage="http://www.videolan.org" autoplay="yes" loop="no" width="300" height="300" target="<?php if($_SESSION['user'] != "admin"){ echo "Video available after login"; } else { echo $a['link'];} ?>" />
 <object classid="clsid:9BE31822-FDAD-461B-AD51-BE1D1C159921" codebase="http://download.videolan.org/pub/videolan/vlc/last/win32/axvlc.cab" style="display:none;"></object>
 </div>
 </div>
