c10f582e720197d0700f09bed0e01891b0ac91a7
root <root@raspberrypi.(none)>
2015-10-06 06:31:44 +0000

    add to modem smsc - smstools

--- modules/settings/modem.php
+++ modules/settings/modem.php
@@ -1,19 +1,10 @@
 <?php
-   if ($_POST['scan'] == "Scan"){
-	   exec("sh modules/settings/modem_scan");   
-  		exec("rm tmp/gammu_identify*");
-		header("location: " . $_SERVER['REQUEST_URI']);
-  		exit();
-   } 
-//   $dd = $_POST["dd"];
-//	if (!empty($dd) && ($_POST['dd1'] == "dd2") ){
-//   	$db = new PDO('sqlite:dbf/nettemp.db');
-//   	$db->exec("DELETE FROM sms_settings WHERE id='$dd'") or die ($db->lastErrorMsg());
-//   	header("location: " . $_SERVER['REQUEST_URI']);
-//   	exit();
-//   }
-      $sd = $_POST["sd"];
-      if ($_POST['sd1'] == "sd2") {
+$dir=$_SERVER["DOCUMENT_ROOT"];
+
+    $sd = $_POST["sd"];
+    $smsc = $_POST["smsc"];
+    if (!empty($smsc) && ($_POST['smsc1'] == "smsc1") ){
+
    	$db = new PDO('sqlite:dbf/nettemp.db');
    	$db->exec("UPDATE sms_settings SET default_dev='off'") or die ($db->lastErrorMsg());
    	$db->exec("UPDATE sms_settings SET default_dev='on' WHERE id='$sd'") or die ($db->lastErrorMsg());
@@ -27,11 +18,32 @@
 				fwrite($fh, "port=$ssd1\n");
 				fwrite($fh, "connection=at\n");
 				fclose ($fh);
-			}
-  		 	exec("gammu -c tmp/gammurc identify > tmp/gammu_identify");
-			header("location: " . $_SERVER['REQUEST_URI']);
-   		exit();
-     }
+				}
+
+    exec("gammu -c tmp/gammurc identify > tmp/gammu_identify");
+    $out = preg_replace('/\//',"\/", $ssd1);
+    $cmd0="sudo cp -f '$dir'/install/conf/smsd.conf /etc/smsd.conf && sudo sed -i s/changedevice/'$out'/g /etc/smsd.conf";
+    passthru($cmd0);
+
+    $cmd1="sudo sed -i s/changesmsc/'$smsc'/g /etc/smsd.conf";
+    passthru($cmd1);
+    shell_exec("sudo service smstools restart");
+
+    header("location: " . $_SERVER['REQUEST_URI']);
+    exit();
+    } 
+
+
+
+   if ($_POST['scan'] == "Scan"){
+	   exec("sh modules/settings/modem_scan");   
+  		exec("rm tmp/gammu_identify*");
+		header("location: " . $_SERVER['REQUEST_URI']);
+  		exit();
+   } 
+      
+
+
     $call = $_POST["call"];
       if ($_POST['call1'] == "call2") {
    	$db = new PDO('sqlite:dbf/nettemp.db');
@@ -82,8 +94,8 @@ $name=$a['name'];
 
 if (!empty($name)) { ?>
 <table><tr><td>
-<form action="" method="post"> 
-<select name="sd"  onchange="this.form.submit()" >
+<form class="form-inline" action="" method="post"> 
+<select name="sd">
 <?php
 $db = new PDO('sqlite:dbf/nettemp.db');
 $sth = $db->prepare("select * from sms_settings ");
@@ -95,7 +107,7 @@ foreach ($result as $a) { ?>
 </select>
 <input type="hidden" name="sd1" value="sd2" />
 </td>
-</form>
+
 <?php
     if (file_exists("tmp/gammu_identify")) { ?>
     <tr><td><pre><?php include('tmp/gammu_identify'); ?></pre></td></tr>
@@ -106,9 +118,32 @@ foreach ($result as $a) { ?>
 
 <?php 
 } 
+$smscff=shell_exec("sudo cat /etc/smsd.conf |grep ^smsc |awk '{print $3}'");
 ?>
+
+
+<fieldset>
+<div class="form-group">
+  <label class="col-md-2 control-label" for="smsc">SMS center number</label>  
+  <div class="col-md-2">
+    <input id="smsc" name="smsc" placeholder="" class="form-control input-md" required="" type="text" value="<?php echo $smscff; ?>">
+  </div>
+</div>
+<br />
+<div class="form-group">
+  <div class="col-md-1">
+    <button id="smsc1" name="smsc1"  value="smsc1" class="btn btn-primary">Save </button>
+  </div>
+</div>
+
+</fieldset>
+</form>
 </div>
 </div>
+
+
+
+
 
 <div class="panel panel-default"><div class="panel-heading">Call modem</div>
 <div class="panel-body">
