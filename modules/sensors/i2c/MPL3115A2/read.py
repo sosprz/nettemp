ce4d3d362587d4e39863afbc44dc5277c1bce4d1
root <root@TEST.(none)>
2015-11-05 18:19:56 +0000

    good driver for MPL3115A2

--- modules/sensors/i2c/MPL3115A2/read.py
+++ modules/sensors/i2c/MPL3115A2/read.py
@@ -14,11 +14,11 @@ ADDR = 0x60
 CTRL_REG1 = 0x26
 PT_DATA_CFG = 0x13
 
-#nbus = sys.argv[1]
-bus = SMBus(1)
+nbus = sys.argv[1]
+bus = SMBus(int(nbus))
 
 who_am_i = bus.read_byte_data(ADDR, 0x0C)
-print hex(who_am_i)
+#print hex(who_am_i)
 if who_am_i != 0xc4:
     print "Device not active."
     exit(1)
@@ -37,18 +37,16 @@ if (setting & 0x02) == 0:
     bus.write_byte_data(ADDR, CTRL_REG1, (setting | 0x02))
 
 # Read sensor data
-print "Waiting for data..."
 status = bus.read_byte_data(ADDR,0x00)
 while (status & 0x08) == 0:
     #print bin(status)
     status = bus.read_byte_data(ADDR,0x00)
     time.sleep(0.5)
 
-print "Reading sensor data..."
 p_data = bus.read_i2c_block_data(ADDR,0x01,3)
 t_data = bus.read_i2c_block_data(ADDR,0x04,2)
 status = bus.read_byte_data(ADDR,0x00)
-print "status: "+bin(status)
+#print "status: "+bin(status)
 
 p_msb = p_data[0]
 p_csb = p_data[1]
@@ -62,7 +60,8 @@ p_decimal = ((p_lsb & 0x30) >> 4)/4.0
 celsius = t_msb + (t_lsb >> 4)/16.0
 fahrenheit = (celsius * 9)/5 + 32
 
-print "Pressure and Temperature at "+time.strftime('%m/%d/%Y %H:%M:%S%z')
-print str(pressure+p_decimal)+" Pa"
-print str(celsius)+deg+"C"
-print str(fahrenheit)+deg+"F"
+#print "Pressure and Temperature at "+time.strftime('%m/%d/%Y %H:%M:%S%z')
+print str(pressure/100+p_decimal)
+#print str(pressure)
+print str(celsius)
+#print str(fahrenheit)+deg+"F"
