3bafbe1bb47f387d61ec579d555db94ec23cf4ff
techfreak <admin@techfreak.pl>
2015-12-03 09:27:50 +0100

    add BH1750

---
+++ modules/sensors/i2c/BH1750/bh1750.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import smbus
+import time
+import sys
+
+# Define some constants from the datasheet
+ 
+DEVICE     = 0x23 # Default device I2C address
+ 
+POWER_DOWN = 0x00 # No active state
+POWER_ON   = 0x01 # Power on
+RESET      = 0x07 # Reset data register value
+ 
+# Start measurement at 4lx resolution. Time typically 16ms.
+CONTINUOUS_LOW_RES_MODE = 0x13
+# Start measurement at 1lx resolution. Time typically 120ms
+CONTINUOUS_HIGH_RES_MODE_1 = 0x10
+# Start measurement at 0.5lx resolution. Time typically 120ms
+CONTINUOUS_HIGH_RES_MODE_2 = 0x11
+# Start measurement at 1lx resolution. Time typically 120ms
+# Device is automatically set to Power Down after measurement.
+ONE_TIME_HIGH_RES_MODE_1 = 0x20
+# Start measurement at 0.5lx resolution. Time typically 120ms
+# Device is automatically set to Power Down after measurement.
+ONE_TIME_HIGH_RES_MODE_2 = 0x21
+# Start measurement at 1lx resolution. Time typically 120ms
+# Device is automatically set to Power Down after measurement.
+ONE_TIME_LOW_RES_MODE = 0x23
+
+busn = sys.argv[1]
+bus = smbus.SMBus(int(busn))
+ 
+def convertToNumber(data):
+  # Simple function to convert 2 bytes of data
+  # into a decimal number
+  return ((data[1] + (256 * data[0])) / 1.2)
+ 
+def readLight(addr=DEVICE):
+  data = bus.read_i2c_block_data(addr,ONE_TIME_HIGH_RES_MODE_1)
+  return convertToNumber(data)
+ 
+def main():
+ 
+     print str(readLight())
+
+   
+if __name__=="__main__":
+   main()
