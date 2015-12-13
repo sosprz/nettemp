a1e20d3cd69d377adef44369a7e7abe7fb82b5f0
techfreak <admin@techfreak.pl>
2015-12-13 10:44:18 +0000

    1wire common

--- modules/settings/settings.php
+++ modules/settings/settings.php
@@ -6,7 +6,7 @@
 <a href="index.php?id=settings&type=accesstime" ><button class="btn <?php echo $art == 'accesstime' ? 'btn-info' : 'btn-default'; ?>">Access time</button></a>
 <a href="index.php?id=settings&type=modem" ><button class="btn <?php echo $art == 'modem' ? 'btn-info' : 'btn-default'; ?>">Modem</button></a>
 <a href="index.php?id=settings&type=gpio" ><button class="btn <?php echo $art == 'gpio' ? 'btn-info' : 'btn-default'; ?>">GPIO</button></a>
-<a href="index.php?id=settings&type=1wire" ><button class="btn <?php echo $art == '1wire' ? 'btn-info' : 'btn-default'; ?>">1wire</button></a>
+<!-- <a href="index.php?id=settings&type=1wire" ><button class="btn <?php echo $art == '1wire' ? 'btn-info' : 'btn-default'; ?>">1wire</button></a> -->
 <a href="index.php?id=settings&type=i2c" ><button class="btn <?php echo $art == 'i2c' ? 'btn-info' : 'btn-default'; ?>">i2c</button></a>
 <a href="index.php?id=settings&type=time" ><button class="btn <?php echo $art == 'time' ? 'btn-info' : 'btn-default'; ?>">Time</button></a>
 <a href="index.php?id=settings&type=snmpd" ><button class="btn <?php echo $art == 'snmpd' ? 'btn-info' : 'btn-default'; ?>">SNMPD</button></a>
