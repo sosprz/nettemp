e33898c540aed6b427fd31185df1d40d3ff2bbee
techfreak <admin@techfreak.pl>
2015-12-26 00:23:15 +0100

    call sms to USB menu

--- modules/settings/modem.php
+++ modules/settings/modem.php
@@ -1,70 +1,68 @@
 <?php
 $dir=$_SERVER["DOCUMENT_ROOT"];
 
-    $sd = $_POST["sd"];
-    $smsc = $_POST["smsc"];
-    if (!empty($smsc) && ($_POST['smsc1'] == "smsc1") ){
+$sd = $_POST["sd"];
 
-   	$db = new PDO('sqlite:dbf/nettemp.db');
-   	$db->exec("UPDATE sms_settings SET default_dev='off'") or die ($db->lastErrorMsg());
-   	$db->exec("UPDATE sms_settings SET default_dev='on' WHERE id='$sd'") or die ($db->lastErrorMsg());
-   	$sth = $db->prepare("SELECT * FROM sms_settings WHERE id='$sd'");
-   	$sth->execute();
-   	$result = $sth->fetchAll();
-			foreach ($result as $sdd) {
-				$ssd1=$sdd['dev'];
-				$fh = fopen('tmp/gammurc', 'w'); 
-				fwrite($fh, "[gammu]\n"); 
-				fwrite($fh, "port=$ssd1\n");
-				fwrite($fh, "connection=at\n");
-				fclose ($fh);
-				}
+if ($_POST['sd1'] == "sd2") {
+    $ssd1=$sd;
+    $fh = fopen('tmp/gammurc', 'w'); 
+    fwrite($fh, "[gammu]\n"); 
+    fwrite($fh, "port=$ssd1\n");
+    fwrite($fh, "connection=at\n");
+    fclose ($fh);
 
     exec("gammu -c tmp/gammurc identify > tmp/gammu_identify");
     $out = preg_replace('/\//',"\/", $ssd1);
     $cmd0="sudo cp -f '$dir'/install/conf/smsd.conf /etc/smsd.conf && sudo sed -i s/changedevice/'$out'/g /etc/smsd.conf";
     passthru($cmd0);
 
+    header("location: " . $_SERVER['REQUEST_URI']);
+    exit();
+}    
+
+
+$smsc = $_POST["smsc"];
+if ($_POST['smsc1'] == "smsc2") {
     $cmd1="sudo sed -i s/changesmsc/'$smsc'/g /etc/smsd.conf";
     passthru($cmd1);
     shell_exec("sudo service smstools restart");
 
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
-    } 
+} 
 
 
 
-   if ($_POST['scan'] == "Scan"){
-	   exec("sh modules/settings/modem_scan");   
-  		exec("rm tmp/gammu_identify*");
-		header("location: " . $_SERVER['REQUEST_URI']);
-  		exit();
-   } 
+//   if ($_POST['scan'] == "Scan"){
+//	   exec("sh modules/settings/modem_scan");   
+//		exec("rm tmp/gammu_identify*");
+//		header("location: " . $_SERVER['REQUEST_URI']);
+// 		exit();
+//   } 
       
 
 
-    $call = $_POST["call"];
-      if ($_POST['call1'] == "call2") {
-   	$db = new PDO('sqlite:dbf/nettemp.db');
-   	$db->exec("UPDATE call_settings SET default_dev='off'") or die ($db->lastErrorMsg());
-   	$db->exec("UPDATE call_settings SET default_dev='on' WHERE id='$call'") or die ($db->lastErrorMsg());
-   	$sth = $db->prepare("SELECT * FROM call_settings WHERE id='$call'");
-   	$sth->execute();
-   	$result = $sth->fetchAll();
-			foreach ($result as $sdd) {
-				$ssd1=$sdd['dev'];
+	$call = $_POST["call"];
+        if ($_POST['call1'] == "call2") {
+   	//$db = new PDO('sqlite:dbf/nettemp.db');
+   	//$db->exec("UPDATE call_settings SET default_dev='off'") or die ($db->lastErrorMsg());
+   	//$db->exec("UPDATE call_settings SET default_dev='on' WHERE id='$call'") or die ($db->lastErrorMsg());
+   	//$sth = $db->prepare("SELECT * FROM call_settings WHERE id='$call'");
+   	//$sth->execute();
+   	//$result = $sth->fetchAll();
+	//		foreach ($result as $sdd) {
+				$ssd1=$call;
 				$fh = fopen('tmp/gammurc', 'w'); 
 				fwrite($fh, "[gammu]\n"); 
 				fwrite($fh, "port=$ssd1\n");
 				fwrite($fh, "connection=at\n");
 				fclose ($fh);
-			}
-  		 	exec("gammu -c tmp/gammurc identify > tmp/gammu_identify_call");
+			//}
+ 	exec("gammu -c tmp/gammurc identify > tmp/gammu_identify_call");
 	shell_exec("sudo touch tmp/reboot");
 	header("location: " . $_SERVER['REQUEST_URI']);
    	exit();
-     } 
+        } 
 
 
 $sms_test = $_POST["sms_test"];
@@ -79,10 +77,6 @@ if  ($_POST['sms_test1'] == "sms_test1") {
     }
 
 ?>
-<form action="" method="post">
-    <button type="submit" name="scan" value="Scan" class="btn btn-primary">Search GSM modem</button>
-</form>
-<br />
 <script type="text/javascript">
 $("button").click(function() {
     var $btn = $(this);
@@ -99,43 +93,33 @@ function submitform()
 <div class="panel-body">
 <?php
 $db = new PDO('sqlite:dbf/nettemp.db');
-$sth = $db->prepare("SELECT * FROM sms_settings where id='2'");
+$sth = $db->prepare("SELECT * FROM usb where device='Modem SMS'");
 $sth->execute();
 $result = $sth->fetchAll();
 foreach ($result as $a) {
-$name=$a['name'];
+$dev=$a['dev'];
 }
 
-if (!empty($name)) { ?>
-<table><tr><td>
-<form class="form-inline" action="" method="post"> 
-<select name="sd">
-<?php
-$db = new PDO('sqlite:dbf/nettemp.db');
-$sth = $db->prepare("select * from sms_settings ");
-$sth->execute();
-$result = $sth->fetchAll();
-foreach ($result as $a) { ?>
-<option <?php echo $a['default_dev'] == 'on' ? 'selected="selected"' : ''; ?> value="<?php echo $a['id']; ?>"><?php echo $a["name"]; ?> <?php echo $a["dev"]; ?> </option>
-<?php } ?>
-</select>
-<input type="hidden" name="sd1" value="sd2" />
-</td>
-
+if (!empty($dev)) { ?>
+    <form class="form-inline" action="" method="post"> 
+	<button class="btn btn-xs btn-success"><?php echo $dev." "?><span class="glyphicon glyphicon-refresh"</span></button>
+	<input type="hidden" name="sd" value="><?php echo $dev ?>" />	
+	<input type="hidden" name="sd1" value="sd2" />
+    </form>
+    <br>
 <?php
     if (file_exists("tmp/gammu_identify")) { ?>
-    <tr><td><pre><?php include('tmp/gammu_identify'); ?></pre></td></tr>
+    <pre><?php include('tmp/gammu_identify'); ?></pre>
 <?php
     }
 ?>
-</tr></table>
 
 <?php 
 } 
 $smscff=shell_exec("sudo cat /etc/smsd.conf |grep ^smsc |awk '{print $3}'");
 ?>
 
-
+<form action="" method="post">
 <fieldset>
 <div class="form-group">
   <label class="col-md-2 control-label" for="smsc">SMS center number</label>  
@@ -143,31 +127,29 @@ $smscff=shell_exec("sudo cat /etc/smsd.conf |grep ^smsc |awk '{print $3}'");
     <input id="smsc" name="smsc" placeholder="" class="form-control input-md" required="" type="text" value="<?php echo $smscff; ?>">
   </div>
 </div>
-<br />
 <div class="form-group">
   <div class="col-md-1">
-    <button id="smsc1" name="smsc1"  value="smsc1" class="btn btn-primary">Save </button>
+    <button id="smsc1" name="smsc1"  value="smsc2" class="btn btn-primary">Save </button>
   </div>
 </div>
 
 </fieldset>
 </form>
 
-<form class="form-horizontal" action="" method="post">
+<form action="" method="post">
 <fieldset>
 
 <!-- Text input-->
 <div class="form-group">
   <label class="col-md-2 control-label" for="sms_test">Number</label>  
   <div class="col-md-2">
-  <input id="sms_test" name="sms_test" placeholder="" class="form-control input-md" required="" type="text" value="">
+    <input id="sms_test" name="sms_test" placeholder="" class="form-control input-md" required="" type="text" value="">
   </div>
 </div>
 
 <!-- Button -->
 <div class="form-group">
-  <label class="col-md-2 control-label" for="sms_test1"></label>
-  <div class="col-md-2">
+  <div class="col-md-1">
     <button id="sms_test1" name="sms_test1" value="sms_test1" class="btn btn-primary">Send test </button>
   </div>
 </div>
@@ -187,39 +169,28 @@ $smscff=shell_exec("sudo cat /etc/smsd.conf |grep ^smsc |awk '{print $3}'");
 <div class="panel-body">
 <?php
 $db = new PDO('sqlite:dbf/nettemp.db');
-$sth = $db->prepare("SELECT * FROM call_settings where id='2'");
+$sth = $db->prepare("SELECT * FROM usb WHERE device='Modem Call'");
 $sth->execute();
 $result = $sth->fetchAll();
 foreach ($result as $a) {
-$name=$a['name'];
+$dev=$a['dev'];
 }
 
-if (!empty($name)) { ?>
-<table><tr><td>
-<form action="" method="post"> 
-<select name="call"  onchange="this.form.submit()" >
-<?php
-$db = new PDO('sqlite:dbf/nettemp.db');
-$sth = $db->prepare("select * from call_settings ");
-$sth->execute();
-$result = $sth->fetchAll();
-foreach ($result as $a) { ?>
-<option <?php echo $a['default_dev'] == 'on' ? 'selected="selected"' : ''; ?> value="<?php echo $a['id']; ?>"><?php echo $a["name"]; ?> <?php echo $a["dev"]; ?> </option>
-<?php } ?>
-</select>
-<input type="hidden" name="call1" value="call2" />
-</td>
-</form>
+if (!empty($dev)) { ?>
+	    <form action="" method="post">
+	    <input type="hidden" name="call1" value="call2" />
+	    <input type="hidden" name="call" value="<?php echo $dev ?>" />
+	    <button class="btn btn-xs btn-success"><?php echo $dev." "?><span class="glyphicon glyphicon-refresh"</span></button>
+	    </form>
+<br>
 <?php
     if (file_exists("tmp/gammu_identify_call")) { ?>
-    <tr><td><pre><?php include('tmp/gammu_identify_call'); ?></pre></td></tr>
+	    <pre><?php include('tmp/gammu_identify_call'); ?> </pre>
 <?php
     }
 ?>
-</tr></table>
-
 <?php 
-} 
+    } 
 ?>
 </div>
 </div>
