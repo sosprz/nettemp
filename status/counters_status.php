6b4e9dc4fa969088dbf079cb3c0b6255d318d619
techfreak <admin@techfreak.pl>
2015-12-29 18:59:38 +0100

    sdm vs status

--- status/counters_status.php
+++ status/counters_status.php
@@ -82,7 +82,7 @@ if ( $numRows > '0' ) { ?>
 	    <small>
 	    <span class="label label-danger">
 		<?php
-		$rows = $dbs->query("SELECT last AS sums FROM def ORDER BY last DESC LIMIT 1") or die('lol');
+		$rows = $dbs->query("SELECT last AS sums FROM def ORDER BY last DESC LIMIT 1");
 		$i = $rows->fetch();
 		if (!empty($i['sums'])){
 		    echo number_format($i['sums'], 2, '.', ',')." ";
