013cbe57f79d9a90e197bb63b6b1296464f1ee3b
root <admin@techfreak.pl>
2016-01-27 20:08:31 +0000

    hcs04

--- modules/sensors/GPIO/HCS04/hcs04.py
+++ modules/sensors/GPIO/HCS04/hcs04.py
@@ -1,52 +1,77 @@
 #!/usr/bin/python
 
-# Author : Matt Hawkins
-# Date   : 09/01/2013
+# Author  : Matt Hawkins
+# Date    : 09/01/2013
+# Modifty : techfreak.pl
+# Date;   : 27/01/2015
+#http://stackoverflow.com/questions/14920384/stop-code-after-time-period
  
 # Import required Python libraries
 import time
 import RPi.GPIO as GPIO
+import multiprocessing
+import time
+
+
+def foo():
+    GPIO.setmode(GPIO.BCM)
+    # Define GPIO to use on Pi
+    GPIO_TRIGGER = 23
+    GPIO_ECHO = 24
+    # Set pins as output and input
+    GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
+    GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+    #Set trigger to False (Low)
+    GPIO.output(GPIO_TRIGGER, False)
  
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
+    # Allow module to settle
+    time.sleep(0.5)
  
-# Define GPIO to use on Pi
-GPIO_TRIGGER = 23
-GPIO_ECHO = 24
+    # Send 10us pulse to trigger
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+    start = time.time()
+    while GPIO.input(GPIO_ECHO)==0:
+	start = time.time()
  
-# Set pins as output and input
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
-GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+    while GPIO.input(GPIO_ECHO)==1:
+	stop = time.time()
  
-# Set trigger to False (Low)
-GPIO.output(GPIO_TRIGGER, False)
+    # Calculate pulse length
+    elapsed = stop-start
  
-# Allow module to settle
-time.sleep(0.5)
+    # Distance pulse travelled in that time is time
+    # multiplied by the speed of sound (cm/s)
+    distance = elapsed * 34000
  
-# Send 10us pulse to trigger
-GPIO.output(GPIO_TRIGGER, True)
-time.sleep(0.00001)
-GPIO.output(GPIO_TRIGGER, False)
-start = time.time()
-while GPIO.input(GPIO_ECHO)==0:
-  start = time.time()
+    # That was the distance there and back so halve the value
+    distance = distance / 2
  
-while GPIO.input(GPIO_ECHO)==1:
-  stop = time.time()
- 
-# Calculate pulse length
-elapsed = stop-start
- 
-# Distance pulse travelled in that time is time
-# multiplied by the speed of sound (cm/s)
-distance = elapsed * 34000
- 
-# That was the distance there and back so halve the value
-distance = distance / 2
- 
-print "%.1f" % distance
- 
-# Reset GPIO settings
-GPIO.cleanup()
+    print "%.1f" % distance
+
+
+
+    # Reset GPIO settings
+    GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    # Start foo as a process
+    p = multiprocessing.Process(target=foo, name="Foo")
+    p.start()
+
+    # Wait 10 seconds for foo
+    time.sleep(1)
+
+    # Terminate foo
+    p.terminate()
+
+    # Cleanup
+    p.join()
+
+
+
+
+
+
