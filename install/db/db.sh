e78a6ec86fcd6bcfe6efbaa9af8c50a6a1e5879c
techfreak <admin@techfreak.pl>
2016-02-16 21:44:26 +0100

    installer

--- install/db/db.sh
+++ install/db/db.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
 $dir/modules/tools/db_reset
-$dir/modules/tools/update_su
-$dir/modules/tools/update_perms
--- install/perms/perms.sh
+++ install/perms/perms.sh
@@ -3,3 +3,7 @@
 umask 002 $dir/tmp
 umask 002 $dir/db
 umask 002 $dir/dbf
+
+$dir/modules/tools/update_su
+$dir/modules/tools/update_perms
+
