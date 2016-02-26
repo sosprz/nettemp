9b9dc5bba80775c39be5c7f69087d37ce5c4de7b
admin <admin@techfreak.pl>
2016-02-26 13:17:51 +0100

    ok

--- modules/gpio/temp.php
+++ modules/gpio/temp.php
@@ -8,79 +8,92 @@ $debug = isset($_GET['debug']) ? $_GET['debug'] : '';
 ////////////////////////////////////////////////////////
 // functions
 
-function w_profile($gpio,$w_profile) {
+function w_profile_check($gpio,$w_profile) {
 	$day=date("D");
-	$time='';
+	$time=date("Hi");
 	$db = new PDO('sqlite:../../dbf/nettemp.db');
 	$rows = $db->query("SELECT * FROM day_plan WHERE name='$w_profile' AND (Mon='$day' OR Tue='$day' OR Wed='$day' OR Thu='$day' OR Fri='$day' OR Sat='$day' OR Sun='$day')");
 	$row = $rows->fetchAll();
+	$numRows = count($row);
+ 	if ( $numRows > '0' ) { 
 		foreach ($row as $b) {
 			$stime=$b['stime'];
 			$etime=$b['etime'];
 		}
-		if($time >= $stimec && $time < $etimec) {
-				echo "onnnnnnnn\n";
+		if($time >= $stime && $time < $etime) {
+				 return true;
 		}
 			else {
-				echo "offff\n";
+				return false;
 			}
-		
+	}
+		else {
+			return false;
+		}
 }
 
-function action_on($op,$sensor_name) {
-	echo $sensor_name." on\n";
+function action_on($op,$sensor_name,$gpio) {
+	echo "GPIO ".$gpio." TRUN ON\n";
+	exit();
 }
-function action_off($op,$sensor_name) {
-	echo $sensor_name." off\n";
+function action_off($op,$sensor_name,$gpio) {
+	echo "GPIO TRUN OFF\n";
+	exit();
 }
 
-
-function temp($op,$sensor_tmpadj,$value,$sensor_name,$op) {
+function temp($op,$sensor_tmpadj,$value,$sensor_name,$op,$gpio) {
 	if ($op=='gt') {
 			if ($sensor_tmpadj > $value){
-				action_on($op,$sensor_name);				
+				action_on($op,$sensor_name,$gpio);				
 			}
 			else {
-				action_off($op,$sensor_name);	
+				action_off($op,$sensor_name,$gpio);	
 			}
 		} 
 		elseif ($op=='ge') {
 			if ($sensor_tmpadj >= $value){
-				action_on($op,$sensor_name);	
+				action_on($op,$sensor_name,$gpio);	
 			}
 			else {
-				 action_off($op,$sensor_name);
+				 action_off($op,$sensor_name,$gpio);
 			}
 		} 
 		elseif ($op=='le') {
 			if ($sensor_tmpadj <= $value){
-				action_on($op,$sensor_name);	
+				action_on($op,$sensor_name,$gpio);	
 			}
 			else {
-				 action_off($op,$sensor_name);
+				 action_off($op,$sensor_name,$gpio);
 			}
 		} 
 		elseif ($op=='lt') {
 			if ($sensor_tmpadj < $value){
-				action_on($op,$sensor_name);	
+				action_on($op,$sensor_name,$gpio);	
 			}
 			else {
-				 action_off($op,$sensor_name);
+				 action_off($op,$sensor_name,$gpio);
 			}
 		}
 }
+
+
+
 		
 // main loop
-$rows = $db->query("SELECT * FROM gpio");
-$row = $rows->fetchAll()
+$rows = $db->query("SELECT * FROM gpio WHERE mode='temp'");
+$row = $rows->fetchAll();
 foreach ($row as $a) {
 	$gpio=$a['gpio'];
+	$rev=$a['rev']; //////////////////////reverse
+		if($rev=='on') {$mode='LOW';} else {$mode='HIGH';}
 	$day_run=$a['day_run'];	
+	echo "GPIO ".$gpio.", Mode=".$mode.", ".date('l jS \of F Y H:i:s')."\n";
 		
 	$rows = $db->query("SELECT * FROM g_func WHERE gpio='$gpio' ORDER BY position ASC");
 	$func = $rows->fetchAll();
 	//get data function for gpio
 	foreach ($func as $func) {
+		   $f_id=$func['id'];
 			$sens_id=$func['sensor'];
 			$sens2_id=$func['sensor2'];
 			$op=$func['op'];
@@ -90,9 +103,24 @@ foreach ($row as $a) {
 			$w_profile=$func['w_profile'];
 			
 			//check if in week profile, if not exit, if empty or in range go forward
-			if($day_run=='on') && ($w_profile !='always') {
-				w_profile($gpio,$w_profile);
+			if(($day_run=='on') && ($w_profile!='any')) {
+				if (w_profile_check($gpio,$w_profile)===false){
+					echo "Function ".$f_id." with profile '".$w_profile."' not in range.\n";
+					continue;
+				} 
+				else {
+					echo "Function ".$f_id." with profile '".$w_profile."' hit.\n";
+				}
 			}
+			//hit in any
+			elseif($w_profile!='any') {
+					echo "Function ".$f_id." with profile '".$w_profile."' not in range. Day off.\n";
+					continue;
+				}
+				else {
+					echo "Function ".$f_id." with profile '".$w_profile."' hit.\n";
+				}
+			
 						
 			$rows = $db->query("SELECT * FROM sensors WHERE id='$sens_id'");
 			$sens = $rows->fetchAll();
@@ -127,12 +155,13 @@ foreach ($row as $a) {
 		
 	
 			if($source=='temp' || $source=='sensor2') {
-				temp($op,$sensor_tmpadj,$value,$sensor_name,$op);		
+				temp($op,$sensor_tmpadj,$value,$sensor_name,$op,$gpio);		
 			} 
 			elseif($source=='temphyst' || $source=='sensor2hyst') {
 				// dkonczyc
 				echo "";
 			}
+			
 	} //function
 }  //main
 ?>
