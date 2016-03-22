118d0fe2ae2d7dfd6626a73731355877ca72b416
admin <admin@techfreak.pl>
2016-03-22 07:48:18 +0100

    F

--- modules/highcharts/html/menu.php
+++ modules/highcharts/html/menu.php
@@ -1,8 +1,8 @@
 <div id="container" style="height: 400 ; min-width: 310px"></div>
-<script type="text/javascript">
+<script type="text/javascript"> 	 
 var start = +new Date();
 function getUrlVars() {
-        var vars = {};
+       var vars = {};
 	    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
 	    vars[key] = value;
     });
@@ -13,29 +13,16 @@ function getUrlVars() {
     var single = getUrlVars()["single"];
     var group = getUrlVars()["group"];
     var mode = getUrlVars()["mode"];
-
-if (type=='temp') { var xval = " °C"}
-if (type=='humid') { var xval = " %"}
-if (type=='press') { var xval = " hPa"}
-if (type=='gpio') { var xval = " H/L"}
-if (type=='host') { var xval = " ms"}
-if (type=='system') { var xval = " %"}
-if (type=='lux') { var xval = " lux"}
-if (type=='water') { var xval = " m3"}
-if (type=='gas') { var xval = " m3"}
-if (type=='elec') { var xval = " kWh"}
-if (type=='elec' && mode=='2') { var xval = " W"}
-if (type=='hosts') { var xval = " ms"}
-if (type=='volt') { var xval = " V"}
-if (type=='amps') { var xval = " A"}
-if (type=='watt') { var xval = " W"}
-if (type=='dist') { var xval = " cm"}
-
-$(function () {
-    var seriesOptions = [],
-        seriesCounter = 0,
-
+    
 <?php
+$dirb = "sqlite:dbf/nettemp.db";
+$dbh = new PDO($dirb) or die("cannot open database");
+$query = "SELECT temp_scale FROM settings WHERE id='1'";
+foreach ($dbh->query($query) as $row) {
+	$temp_scale=$row['temp_scale'];
+}
+echo "temp_scale = '". $temp_scale ."';\n";
+
 parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $url);
 $type=$url['type'];
 $single=$url['single'];
@@ -121,6 +108,36 @@ echo "names = ". $js_array .";\n";
 $types = json_encode($types);
 echo "types = ". $types .";\n";
 ?>
+
+    if (type=='temp' && temp_scale=='F') { 
+		    		var xvall = " °F"
+		    		}
+		    		else {
+		    			var xvall = " °C"
+		    		}
+if (type=='humid') { var xval = " %"}
+if (type=='press') { var xval = " hPa"}
+if (type=='gpio') { var xval = " H/L"}
+if (type=='host') { var xval = " ms"}
+if (type=='system') { var xval = " %"}
+if (type=='lux') { var xval = " lux"}
+if (type=='water') { var xval = " m3"}
+if (type=='gas') { var xval = " m3"}
+if (type=='elec') { var xval = " kWh"}
+if (type=='elec' && mode=='2') { var xval = " W"}
+if (type=='hosts') { var xval = " ms"}
+if (type=='volt') { var xval = " V"}
+if (type=='amps') { var xval = " A"}
+if (type=='watt') { var xval = " W"}
+if (type=='dist') { var xval = " cm"}
+
+
+
+$(function () {
+    var seriesOptions = [],
+        seriesCounter = 0,
+
+
 	
 
         // create the chart when all data is loaded
@@ -161,24 +178,7 @@ echo "types = ". $types .";\n";
 		y: 0,
         	labelFormatter: function() {
             	    var lastVal = this.yData[this.yData.length - 1];
-		    if (types[this.name]=='temp') { var xvall = " °C"};
-		    if (types[this.name]=='humid') { var xvall = " %"};
-		    if (types[this.name]=='press') { var xvall = " hPa"};
-		    if (types[this.name]=='gpio') { var xvall = " H/L"};
-		    if (types[this.name]=='host') { var xvall = " ms"};
-		    if (types[this.name]=='system') { var xvall = " %"};
-		    if (types[this.name]=='lux') { var xvall = " lux"};
-		    if (types[this.name]=='water') { var xvall = " m3"};
-		    if (types[this.name]=='gas') { var xvall = " m3"};
-	    	    if (types[this.name]=='elec') { var xvall = " kWh"};
-		    if (types[this.name]=='elec' && mode=='2') { var xvall = " W"};
-		    if (types[this.name]=='hosts') { var xvall = " ms"};
-		    if (types[this.name]=='volt') { var xvall = " V"};
-		    if (types[this.name]=='amps') { var xvall = " A"};
-		    if (types[this.name]=='watt') { var xvall = " W"};
-		    if (types[this.name]=='dist') { var xvall = " cm"};
-
-		    return '<span style="color:' + this.color + '">' + this.name + ': </span> <b>' + lastVal + xvall +'</b> </n>';
+		    			 return '<span style="color:' + this.color + '">' + this.name + ': </span> <b>' + lastVal + xvall +'</b> </n>';
         	    }
 		},
 
@@ -203,23 +203,6 @@ echo "types = ". $types .";\n";
         };
 
     $.each(names, function (i, name) {
-
-    if (types[name]=='temp') { var xvall = " °C"}
-    if (types[name]=='humid') { var xvall = " %"}
-    if (types[name]=='press') { var xvall = " hPa"}
-    if (types[name]=='gpio') { var xvall = " H/L"}
-    if (types[name]=='host') { var xvall = " ms"}
-    if (types[name]=='system') { var xvall = " %"}
-    if (types[name]=='lux') { var xvall = " lux"}
-    if (types[name]=='water') { var xvall = " m3"}
-    if (types[name]=='gas') { var xvall = " m3"}
-    if (types[name]=='elec') { var xvall = " kWh"}
-    if (types[name]=='elec' && mode=='2') { var xvall = " W"}
-    if (types[name]=='hosts') { var xvall = " ms"}
-    if (types[name]=='volt') { var xvall = " V"}
-    if (types[name]=='amps') { var xvall = " A"}
-    if (types[name]=='watt') { var xvall = " W"}
-    if (types[name]=='dist') { var xvall = " cm"}
 
         $.getJSON('common/hc_data.php?type='+type+'&name='+name+'&max='+max+'&mode='+mode,  function (data) {
 
@@ -272,8 +255,6 @@ echo "types = ". $types .";\n";
 		
 		
         }
-	    
-
             // As we're loading the data asynchronously, we don't know what order it will arrive. So
             // we keep a counter and create the chart when all the data is loaded.
             seriesCounter += 1;
