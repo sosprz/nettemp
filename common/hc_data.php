62cd6f1eae0538053c6fd4d9beb7aa370f1ee3e2
admin <admin@techfreak.pl>
2016-03-25 12:53:31 +0100

    hc

--- common/hc_data.php
+++ common/hc_data.php
@@ -16,16 +16,16 @@ if ($max == 'day') {
       $query = "select strftime('%s', time),value from def WHERE time >= datetime('now','localtime','-1 day')";
     } 
 if ($max == 'week') {
-    $query = "select strftime('%s', time),value from def WHERE time >= datetime('now','localtime','-7 day')";
+    $query = "select strftime('%s', time),value from def WHERE time >= datetime('now','localtime','-7 day') AND rowid % 100=0";
     } 
 if ($max == 'month') {
-    $query = "select strftime('%s', time),value from def WHERE time >= datetime('now','localtime','-1 months')";
+    $query = "select strftime('%s', time),value from def WHERE time >= datetime('now','localtime','-1 months') AND rowid % 100=0";
     } 
 if ($max == 'months') {
-    $query = "select strftime('%s', time),value from def WHERE time >= datetime('now','localtime','-6 months')";
+    $query = "select strftime('%s', time),value from def WHERE time >= datetime('now','localtime','-6 months') AND rowid % 200=0";
     } 
 if ($max == 'year') {
-    $query = "select strftime('%s', time),value from def WHERE time >= datetime('now','localtime','-1 year')";
+    $query = "select strftime('%s', time),value from def WHERE time >= datetime('now','localtime','-1 year') AND rowid % 400=0";
     } 
 if ($max == 'all') {
     $query = "select strftime('%s', time),value FROM def";
