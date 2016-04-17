0288e1cfb3e5292b30604735689310c06bb86952
admin <admin@techfreak.pl>
2016-04-17 18:38:08 +0200

    DBedit in tools

--- modules/tools/html/tools_db_edit_select.php
+++ modules/tools/html/tools_db_edit_select.php
@@ -4,6 +4,14 @@ $filee = isset($_POST['filee']) ? $_POST['filee'] : '';
 $save = isset($_POST['save']) ? $_POST['save'] : '';
 $value = isset($_POST['value']) ? $_POST['value'] : '';
 $id = isset($_POST['id']) ? $_POST['id'] : '';
+$replace= isset($_POST['replace']) ? $_POST['replace'] : '';
+$sreplace= isset($_POST['sreplace']) ? $_POST['sreplace'] : '';
+$rreplace= isset($_POST['rreplace']) ? $_POST['rreplace'] : '';
+$search= $_POST['search'];
+//$search= isset($_POST['search']) ? $_POST['search'] : '';
+if(empty($search)) {
+	$search= isset($_GET['search']) ? $_GET['search'] : '';
+}
 if (($save == "save")){
     $db = new PDO("sqlite:db/$filee.sql");
     $db->exec("UPDATE def SET value='$value' WHERE rowid='$id'");
@@ -11,7 +19,32 @@ if (($save == "save")){
     exit();
 }
 
+if ($replace == "set"){
+    $db = new PDO("sqlite:db/$filee.sql");
+    $db->exec("UPDATE def SET value='$rreplace' WHERE value='$sreplace'");
+    header("location: " . $_SERVER['REQUEST_URI']);
+    exit();
+}
+
 ?>
+<div class="panel panel-default">
+<div class="panel-heading">Search in <?php echo $file.".sql" ?></div>
+<div class="panel-body">
+	<form action="" method="post" style="display:inline!important;">
+      Search value: <input type="input" size="4" name="search" value="<?php echo $search ?>" />
+    	<button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-plus"></span></button>
+    	<input type="hidden" name="save" value="empty" />
+    </form>
+    <form action="" method="post">
+      <input type="hidden" name="filee" value="<?php echo $file ?>" />
+      Search value: <input type="input" size="4" name="sreplace" value="" />
+      replace: <input type="input" size="4" name="rreplace" value="" />
+    	<button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-plus"></span></button>
+    	<input type="hidden" name="replace" value="set" />
+    </form>
+</div>
+</div>
+
 <div class="panel panel-default">
 <div class="panel-heading">Edit: <?php echo $file.".sql" ?></div>
 
@@ -21,7 +54,12 @@ $pstop=100;
 $pstart = ($page-1) * $pstop; 
 
 $db = new PDO("sqlite:db/$file.sql");
-$rows = $db->query("SELECT rowid,* FROM def Limit $pstart, $pstop");
+if(empty($search)) {
+	$rows = $db->query("SELECT rowid,* FROM def Limit $pstart, $pstop");
+}
+else {
+	$rows = $db->query("SELECT rowid,* FROM def WHERE value='$search' Limit $pstart, $pstop");
+}
 $row = $rows->fetchAll();
 ?>
 <table class="table table-striped table-hover small">
@@ -56,19 +94,29 @@ $row = $rows->fetchAll();
 
 </table>
 <?php
-	echo "<a href='index.php?id=tools&type=dbedit2&file=$file&page=1'>".'|<'."</a> ";
 	
-	$rows = $db->query("SELECT * FROM def");
+	
+	if(empty($search)) {
+		$rows = $db->query("SELECT * FROM def");
+	}
+	else {
+		$rows = $db->query("SELECT * FROM def WHERE value='$search'");
+	}
 	$row = $rows->fetchAll();
 	$total_records = count($row);
 	
+	if($total_records >=101) {
+	
 	$total_pages = ceil($total_records / $pstop); 
 	
+	echo "<a href='index.php?id=tools&type=dbedit2&file=$file&search=$search&page=1'>".'|<'."</a> ";
+	
 	for ($i=1; $i<=$total_pages; $i++) { 
-            echo "<a href='index.php?id=tools&type=dbedit2&file=$file&page=".$i."'>".$i."</a> "; 
+            echo "<a href='index.php?id=tools&type=dbedit2&file=$file&search=$search&page=".$i."'>".$i."</a> "; 
 	}; 	
 	
-	echo "<a href='index.php?id=tools&type=dbedit2&file=$file&page=$total_pages'>".'>|'."</a> "; 
+	echo "<a href='index.php?id=tools&type=dbedit2&file=$file&search=$search&page=$total_pages'>".'>|'."</a> "; 
 	
+}
 	?>
 </div>
