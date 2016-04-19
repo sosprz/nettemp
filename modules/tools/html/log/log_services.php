d278d5d452e1d9e18dc00e3ecd8b0f11776b0d5b
admin <admin@techfreak.pl>
2016-04-19 22:52:22 +0200

    log services

--- modules/tools/html/log/log_services.php
+++ modules/tools/html/log/log_services.php
@@ -1,16 +1,20 @@
+<?php
+$service=isset($_GET['service']) ? $_GET['service'] : '';
+?>
 <p>
-    <a href="index.php?id=tools&type=log&log=services&service=smstools"><button class="btn btn-default btn-xs">SMStools </button></a>
-    <a href="index.php?id=tools&type=log&log=services&service=msmtp"><button class="btn btn-default btn-xs">msmtp </button></a>
+    <a href="index.php?id=tools&type=log&log=services&service=smstools" class="btn btn-default btn-xs" <?php echo $service == 'smstools' ? 'active' : ''; ?> ">SMStools </a>
+    <a href="index.php?id=tools&type=log&log=services&service=msmtp" class="btn btn-default btn-xs" <?php echo $service == 'msmtp' ? 'active' : ''; ?> ">msmtp </a>
+    <a href="index.php?id=tools&type=log&log=services&service=lighttpd" class="btn btn-default btn-xs" <?php echo $service == 'lighttpd' ? 'active' : ''; ?> ">lighttpd </a>
 </p>
 
 <pre>
 <?php
-$service=isset($_GET['service']) ? $_GET['service'] : '';
 switch ($service)
 { 
 default: case '$service': include('/var/log/msmtp.log'); break;
 case 'msmtp': include('/var/log/msmtp.log'); break;
 case 'smstools': include('/var/log/smstools/smsd.log'); break;
+case 'lighttpd': include('/var/log/lighttpd/error.log'); break;
 
 }
 ?>
