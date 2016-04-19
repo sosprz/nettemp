6e07747a738805a9333affed28d11fdeb0e002aa
admin <admin@techfreak.pl>
2016-04-19 22:30:17 +0200

    log services

--- modules/tools/html/log/log_services.php
+++ modules/tools/html/log/log_services.php
@@ -2,13 +2,16 @@
     <a href="index.php?id=tools&type=log&log=services&service=smstools"><button class="btn btn-default btn-xs">SMStools </button></a>
     <a href="index.php?id=tools&type=log&log=services&service=msmtp"><button class="btn btn-default btn-xs">msmtp </button></a>
 </p>
+
+</pre>
 <?php
 $service=isset($_GET['service']) ? $_GET['service'] : '';
 switch ($log)
 { 
 default: case '$service': include('/var/log/msmtp.log'); break;
-case 'msmtp': include('mo/var/log/msmtp.log'); break;
+case 'msmtp': include('/var/log/msmtp.log'); break;
 case 'smstools': include('/var/log/smstools/smsd.log'); break;
 
 }
 ?>
+</pre>
