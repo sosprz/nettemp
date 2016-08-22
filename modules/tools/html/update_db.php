328270efb47467d6759cf7c58bf61d06f71ca5d9
admin <admin@techfreak.pl>
2016-08-22 19:50:58 +0200

    types table

--- modules/tools/html/update_db.php
+++ modules/tools/html/update_db.php
@@ -269,6 +269,9 @@ $db->exec("INSERT OR IGNORE INTO html (name, state) VALUES ('screen', 'off')");
 $db->exec("CREATE TABLE minmax (id INTEGER PRIMARY KEY,name UNIQUE,state TEXT,value TEXT)");
 $db->exec("INSERT OR IGNORE INTO minmax (name, state) VALUES ('mode', '1')");
 
+$db->exec("CREATE TABLE types (id INTEGER PRIMARY KEY,type UNIQUE, unit TEXT, ico TEXT, title TEXT)");
+$db->exec("INSERT OR IGNORE INTO types (type, unit, ico, title) VALUES ('temp', 'C', 'media/ico/temp2-icon.png' ,'Temperature')");
+
 
 
 echo "ok";
--- status/sensor_status.php
+++ status/sensor_status.php
@@ -75,8 +75,6 @@ Go to device scan!
 		
 		//glyphicon glyphicon-exclamation-sign	
 		
-		//if($a['tmp'] > $a['tmp_5ago']) { $updo='<img src="media/ico/Up-3-icon.png"/>';}
-		//if($a['tmp'] < $a['tmp_5ago']) { $updo='<img src="media/ico/Down-3-icon.png" />';}
 		if($a['tmp'] > $a['tmp_5ago']) { $updo='<span class="label label-danger" title='.$a['tmp_5ago'].'><span class="glyphicon glyphicon-arrow-up"></span></span>';}
 		if($a['tmp'] < $a['tmp_5ago']) { $updo='<span class="label label-info" title='.$a['tmp_5ago'].'><span class="glyphicon glyphicon-arrow-down"></span></span>';}
 		
