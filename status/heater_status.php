414435cba27d58a902c619383629729b3b9a01b2
mariano78 <mariano78@interia.pl>
2016-09-27 15:39:47 +0200

    DB11

--- status/heater_status.php
+++ status/heater_status.php
@@ -29,7 +29,7 @@ if ( $o == 'off') { $rs='OFF'; }
     <td>	<img type="image" src="media/ico/SMD-64-pin-icon_24.png" /></td>
     <td><?php echo $a['name']; ?></td>
 	<td><?php echo $a['temp_actual']; ?></td>
-	d><?php echo $a['temp_set']; ?></td>
+	<td><?php echo $a['temp_set']; ?></td>
     <td><?php echo $a['status']; ?></td>
 	<td><?php echo $a['work_mode']; ?></td>
     <td><?php echo $rs; ?></td>
