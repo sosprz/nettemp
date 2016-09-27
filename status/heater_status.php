16e5c0e90142f8e904b4c765c125f4307c68f2ea
mariano78 <mariano78@interia.pl>
2016-09-27 15:30:37 +0200

    db7

--- status/heater_status.php
+++ status/heater_status.php
@@ -12,11 +12,7 @@ $numRows = count($result);
 <div class="panel panel-default">
             <div class="panel-heading">WiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi HeatersWiFi Heaters</div>
 <table class="table table-hover table-condensed">
-<?php
 
-
-
-?>
     <tr>
     <td>	<img type="image" src="media/ico/SMD-64-pin-icon_24.png" /></td>
     <td><?php echo $a['name']; ?></td>
