1dfe7068b239b760c16a9a3cb3814b554a217641
mariano78 <mariano78@interia.pl>
2016-09-27 14:47:07 +0200

    db

--- status/heater_status.php
+++ status/heater_status.php
@@ -13,22 +13,14 @@ $numRows = count($result);
             <div class="panel-heading">WiFi Heaters</div>
 <table class="table table-hover table-condensed">
 <?php
-foreach ( $result as $a) {
-$ip=$a['ip'];
-$cmd="curl --connect-timeout 3 $ip/showstatus";
-exec($cmd, $i);
-$s=$i[0];
-$o1=str_replace('status', '', $s);
-$o = str_replace(' ', '', $o1);
-if ( $o == 'on') { $rs='ON'; }
-if ( $o == 'off') { $rs='OFF'; }
+
 
 
 ?>
     <tr>
     <td>	<img type="image" src="media/ico/SMD-64-pin-icon_24.png" /></td>
     <td><?php echo $a['name']; ?></td>
-    <td><?php echo $a['status']; ?></td>
+    <td><?php echo $a['work_mode']; ?></td>
     <td><?php echo $rs; ?></td>
     
     </tr>
