64404a85c9130117cc382efca67092e94cb494e6
mariano78 <mariano78@interia.pl>
2016-09-30 15:59:09 +0200

    Name

--- modules/heaters/html/heaters_settings.php
+++ modules/heaters/html/heaters_settings.php
@@ -1,5 +1,5 @@
 <div class="panel panel-default">
-<div class="panel-heading">Heaters</div>
+<div class="panel-heading">WiFiHeaters</div>
 <div class="table-responsive">
 <table class="table table-hover table-condensed small" border="0">
 
