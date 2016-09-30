93fa3a35b21a3db13e52233b5fbc9cd66bddd2a1
mariano78 <mariano78@interia.pl>
2016-09-30 11:44:38 +0200

    ASD

--- modules/heaters/html/heaters_settings.php
+++ modules/heaters/html/heaters_settings.php
@@ -73,13 +73,20 @@ $row = $sth2->fetchAll();
 	<td class="col-md-0">
 	<form action="" method="post" style="display:inline!important;"> 
 		<input type="hidden" name="h2rom" value="<?php echo $a["rom"]; ?>" />
-		<input type="text" name="tempset" size="10" value="<?php echo $a["temp_set"]; ?>" 
-		<input type="hidden" name="hchg" value="hchg" />
+		<input type="text" name="tempset" size="10" value="<?php echo $a["temp_set"]; ?>" /
 		<button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-pencil"></span> </button>
+		<input type="hidden" name="hchg" value="hchg" />
+		
     </form>
 	</td>
 	
-	
+	<td class="col-md-0">
+    <?php if ($a["device"] != 'remote') { ?>
+    <form action="" method="post" style="display:inline!important;">
+	<input type="text" name="adj" size="2" maxlength="30" value="<?php echo $a["adj"]; ?>" required="" <?php echo $a["device"] == 'remote' ? 'disabled' : ''; ?> />
+	<button class="btn btn-xs btn-success"><span class="glyphicon glyphicon-pencil"></span> </button>
+	<input type="hidden" name="name_id" value="<?php echo $a["id"]; ?>" />
+	<input type="hidden" name="adj1" value="adj2"/>
 	
 	
 	
