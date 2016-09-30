bdcc531b8c9f84df9348843d7a2fd8c5d430a830
mariano78 <mariano78@interia.pl>
2016-09-30 15:39:55 +0200

    small table

--- status/heater_status.php
+++ status/heater_status.php
@@ -11,7 +11,7 @@ $numRows = count($result);
 <div class="grid-item heat">
 <div class="panel panel-default">
             <div class="panel-heading">WiFi Heaters</div>
-<table class="table table-responsive table-hover table-condensed">
+<table class="table table-responsive table-hover table-condensed small">
 
 <thead>
 <tr>
