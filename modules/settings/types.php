da75dbae74d8bb3f48026ce8f2f565687ea8c0cc
mariano78 <mariano78@interia.pl>
2016-10-14 14:23:38 +0200

    Types

--- modules/settings/types.php
+++ modules/settings/types.php
@@ -30,7 +30,7 @@
     $db = new PDO("sqlite:dbf/nettemp.db");	
     $db->exec("DELETE from types") or header("Location: html/errors/db_error.php");
     $db->exec("INSERT OR IGNORE INTO types (type, unit, unit2, ico, title) VALUES ('temp', '°C', '°F', 'media/ico/temp2-icon.png' ,'Temperature')");
-	$db->exec("INSERT OR IGNORE INTO types (type, unit, unit2, ico, title) VALUES ('heaterst', '°C', '°F', 'media/ico/heaters-icon.png' ,'Heaters_Temp')");
+	$db->exec("INSERT OR IGNORE INTO types (type, unit, unit2, ico, title) VALUES ('heaterst', '°C', '°F', 'media/ico/heaters-icon.png' ,'Heater')");
 	$db->exec("INSERT OR IGNORE INTO types (type, unit, unit2, ico, title) VALUES ('lux', 'lux', 'lux', 'media/ico/sun-icon.png' ,'Lux')");
 	$db->exec("INSERT OR IGNORE INTO types (type, unit, unit2, ico, title) VALUES ('humid', '%', '%', 'media/ico/rain-icon.png' ,'Humidity')");
     $db->exec("INSERT OR IGNORE INTO types (type, unit, unit2, ico, title) VALUES ('press', 'hPa', 'hPa', 'media/ico/Science-Pressure-icon.png' ,'Pressure')");
