c6162de5cc0a6f77c5a6d1f8aa4405e5fc92899a
mariano78 <mariano78@interia.pl>
2016-10-14 09:17:44 +0200

    sensors AND

--- modules/sensors/html/sensors.php
+++ modules/sensors/html/sensors.php
@@ -84,7 +84,7 @@ $name_new=trim($name_new2);
  
 	
 	//DB    
-	if (($type != "relay") || ($type != "heaterst") ) {
+	if (($type != "relay") AND ($type != "heaterst") ) {
 		$db->exec("INSERT OR IGNORE INTO sensors (name, rom, type, alarm, tmp, gpio, device, method, ip, adj, charts, sum, map_pos, map_num, position, map) VALUES ('$name','$id_rom_new', '$type', 'off', 'wait', '$gpio', '$device', '$method', '$ip', '0', 'on', '0', '{left:0,top:0}', '$map_num', '1', 'on')") or die ("cannot insert to DB" );
 		//maps settings
 		$inserted=$db->query("SELECT id FROM sensors WHERE rom='$id_rom_new'");
@@ -103,7 +103,7 @@ $name_new=trim($name_new2);
 	    }
 	
 	// ADD DB
-   if ($device == "wireless" || $device == "wifiheaters2") {
+   if ($device == "wireless" || $device == "wifiheaters") {
 		//host for monitoring
 		$name='host_wifi_' . $type . '_' . $name;
 		$dbhost = new PDO("sqlite:dbf/nettemp.db");	
