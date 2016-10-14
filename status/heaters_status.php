d6bf7fe04e8448c1668a4efb63998b9382487387
mariano78 <mariano78@interia.pl>
2016-10-14 13:55:28 +0200

    name

--- status/heaters_status.php
+++ status/heaters_status.php
@@ -7,12 +7,6 @@ $sth->execute();
 $result = $sth->fetchAll();
 $numRows = count($result);
 
-//$sth2 = $db->prepare("SELECT * FROM sensors WHERE type = 'heaterst' ");
-//$sth2->execute();
-//$result = $sth2->fetchAll();
-
-
-
 ?>
 <?php if ( $numRows > '0' ) { ?>
 <div class="grid-item heat">
@@ -41,7 +35,7 @@ foreach ( $result as $a) {
 ?>
     <tr>
     <td><small>	<img type="image" src="media/ico/heat.png" /></small></td>
-    <td><small><?php echo $a['name']; ?><small></td>
+    <td><small><?php echo $name; ?><small></td>
 	<td><small><span class="label label-success"><?php echo $a['temp_actual']; ?></span></small></td>
 	<td><small><span class="label label-info"> <?php echo $a['temp_set']; ?></span></small></td>
     <td><small><span class="label label-warning"> <?php echo $a['work_mode']; ?></span></small></td>
