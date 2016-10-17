cb1f55458cde3cac498d608824745530418b895d
mariano78 <mariano78@interia.pl>
2016-10-17 11:12:17 +0200

    eeeeeeeeeeeeee

--- receiver.php
+++ receiver.php
@@ -277,7 +277,7 @@ function db($rom,$val,$type,$device,$current) {
 					
 					if ($type == 'heaters') {
 					$db->exec("UPDATE heaters SET temp_actual='$val' WHERE rom='$rom'") or die ("cannot insert to heaters\n" );
-					echo "$rom ok insert heaters temp_actual $val2 \n";
+					echo "$rom ok insert heaters temp_actual \n";
 					}
 					
 					
