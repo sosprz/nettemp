b3567477f507f68c2562675f2cdbbef55327c6c3
admin <admin@techfreak.pl>
2016-11-30 22:25:30 +0100

    update node php

--- install/apt/apt.sh
+++ install/apt/apt.sh
@@ -5,7 +5,7 @@ echo "Please wait installing packages"
 package="lighttpd php5-cgi php5-sqlite php5-fpm sqlite3 msmtp digitemp gammu git-core mc sysstat \
     sharutils bc htop snmp sudo ntp watchdog python-smbus i2c-tools openvpn iptables rcconf \
     arp-scan snmpd httping fping make gcc lynx expect socat build-essential python-dev figlet \
-    libmodbus5 netfilter-persistent apcupsd smstools php5-mysql mysql-client python-pymodbus"
+    libmodbus5 netfilter-persistent apcupsd smstools php5-mysql mysql-client python-pymodbus php5-curl"
 
 apt-get -y update
 apt-get -y install $package
