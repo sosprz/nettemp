fa095f29f1233127eaa84094f21fd3666620f2f6
admin <admin@techfreak.pl>
2016-11-30 19:39:43 +0100

    mysql update

--- modules/mysql/mysql_send.php
+++ modules/mysql/mysql_send.php
@@ -1,28 +1,28 @@
 <?php
+$date = date("Y-m-d H:i:s"); 
+$conf="../../modules/mysql/mysql_conf.php";
+if(file_exists($conf)) {
+	include_once($conf);
+	$conn = new mysqli($IP, $USER, $PASS, $DB, $PORT);
 
-include_once('../../modules/mysql/mysql_conf.php');
-$conn = new mysqli($IP, $USER, $PASS, $DB, $PORT);
-
-$db = new PDO('sqlite:../../dbf/nettemp.db');
-$rows = $db->query("SELECT * FROM sensors");
-$row = $rows->fetchAll();
-foreach ($row as $a) { 	
-
-$rom=$a['rom'];
-$tmp=$a['tmp'];
-$name=$a['name'];
-
-$sql="INSERT INTO `".$rom."` (value) VALUES ('$tmp')";
-
-
-if ($conn->query($sql) === TRUE) {
-    	echo "Send $tmp to $name successfully\n";
-	} else {
-   	 echo "Error send $tmp to $name\n" . $conn->error;
-	}		
-	
-	
+	$db = new PDO('sqlite:../../dbf/nettemp.db');
+	$rows = $db->query("SELECT * FROM sensors");
+	$row = $rows->fetchAll();
+	foreach ($row as $a) { 	
+		$rom=$a['rom'];
+		$tmp=$a['tmp'];
+		$name=$a['name'];
+		$sql="INSERT INTO `".$rom."` (value) VALUES ('$tmp')";
+		if ($conn->query($sql) === TRUE) {
+			echo "Send $tmp to $name successfully\n";
+		} else {
+			echo "Error send $tmp to $name\n" . $conn->error;
+		}		
 	$sql='';
+	}
+	$conn->close();
+} else {
+	echo $date." No mysql_conf.php\n";
 }
-$conn->close();
+
 ?>
