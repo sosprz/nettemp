8027a025ae654b80ad5bb7e9c65cde4970e7328c
techfreak <admin@techfreak.pl>
2016-12-03 16:26:20 +0000

    update

--- modules/sensors/rs485/sdm630.php
+++ modules/sensors/rs485/sdm630.php
@@ -13,34 +13,30 @@ try {
 }
 
 try {
-	include("$ROOT/receiver.php");
+    include("$ROOT/receiver.php");
     $query = $db->query("SELECT dev FROM usb WHERE device='RS485'");
     $result= $query->fetchAll();
     foreach($result as $r) {
 		$dev=$r['dev'];
-		//$dev='/dev/ttyUSB0'; //
     }
     if($dev=='none'){
 		echo $date." No RS485 USB Device.\n";
 		exit;
 	}
     $dev=str_replace("/dev/","",$dev);
-    echo $dev."\n";
 	$query = $db->query("SELECT addr FROM rs485 WHERE dev='SDM630'");
 	$result= $query->fetchAll();
-	$result=array('1');
     foreach($result as $r) {
-		$addr=$r['addr'];
-		//$addr='1'; //
-    
+	$addr=$r['addr'];
+	echo $date." RS485 ".$dev." ".$addr."\n";
+
     	$cmd="$ROOT/modules/sensors/rs485/sdm630_get.sh /dev/$dev $addr";
 		$res=shell_exec($cmd);
 		$res = preg_split ('/$\R?^/m', $res);
 		foreach ($res as $l) {
-			$line[]=$l;
+			$line[]=trim($l);
 		}
 			
-		//var_dump($line);
 		//L1
 		$local_type='volt';
 		$local_rom="usb_".$dev."L1a".$addr."_".$local_type;
@@ -124,24 +120,21 @@ try {
 		$VAL=$ALL-$last;
 		
 		
-		echo "1. ".$last."\n";
-		echo "2. ".$WATsum."\n";
-		echo "3. ".$ALL."\n";
-		echo "4. ".$VAL."\n";
+		//echo "1. ".$last."\n";
+		//echo "2. ".$WATsum."\n";
+		//echo "3. ".$ALL."\n";
+		//echo "4. ".$VAL."\n";
 		
 		if($last!=0){
 			$local_val=$VAL;
 			$current=$WATsum;
 			db($local_rom,$local_val,$local_type,$device,$current);
 			$db->exec("UPDATE sensors SET sum='$ALL' WHERE rom='$local_rom'");
-			echo $local_val."\n";
 		} else {
 			$local_val='0';
 			$current='';
 			db($local_rom,$local_val,$local_type,$device,$current);
 			$db->exec("UPDATE sensors SET sum='$ALL' WHERE rom='$local_rom'");
-			echo "zzzz";
-			
 		}
 	}
 
