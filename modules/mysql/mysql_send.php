58a8cf3bb10b4acc3764f5a02ce5a7fc9e7adadf
admin <admin@techfreak.pl>
2016-12-04 15:44:18 +0100

    update

--- modules/mysql/mysql_send.php
+++ modules/mysql/mysql_send.php
@@ -8,23 +8,25 @@ $conf="$ROOT/modules/mysql/mysql_conf.php";
 if(file_exists($conf)) {
 	include_once($conf);
 	$conn = new mysqli($IP, $USER, $PASS, $DB, $PORT);
-
-	$db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
-	$rows = $db->query("SELECT * FROM sensors");
-	$row = $rows->fetchAll();
-	foreach ($row as $a) { 	
-		$rom=$a['rom'];
-		$tmp=$a['tmp'];
-		$name=$a['name'];
-		$sql="INSERT INTO `".$rom."` (value) VALUES ('$tmp')";
-		if ($conn->query($sql) === TRUE) {
-			echo "Send $tmp to $name successfully\n";
-		} else {
-			echo "Error send $tmp to $name\n" . $conn->error;
-		}		
-	$sql='';
+	
+	if($conn){
+		$db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
+		$rows = $db->query("SELECT * FROM sensors");
+		$row = $rows->fetchAll();
+		foreach ($row as $a) { 	
+			$rom=$a['rom'];
+			$tmp=$a['tmp'];
+			$name=$a['name'];
+			$sql="INSERT INTO `".$rom."` (value) VALUES ('$tmp')";
+			if ($conn->query($sql) === TRUE) {
+				echo "Send $tmp to $name successfully\n";
+			} else {
+				echo "Error send $tmp to $name\n" . $conn->error;
+			}		
+			$sql='';
+		}
+		$conn->close();
 	}
-	$conn->close();
 } else {
 	echo $date." No mysql_conf.php\n";
 }
