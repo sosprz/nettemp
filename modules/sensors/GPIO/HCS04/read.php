8ca25339dbaf9be8994bfede56e99e60cfbbd324
admin <admin@techfreak.pl>
2016-12-07 18:09:14 +0100

    HCS04 php

---
+++ modules/sensors/GPIO/HCS04/read.php
@@ -0,0 +1,37 @@
+<?php
+$ROOT=dirname(dirname(dirname(dirname(dirname(__FILE__)))));
+ 
+define("LOCAL","local");
+$date = date("Y-m-d H:i:s"); 
+
+
+try {
+    $db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo $date." Could not connect to the database.\n";
+    exit;
+}
+
+try {
+	include("$ROOT/receiver.php");
+    $query = $db->query("select mode from gpio WHERE gpio='23' or gpio='24' and mode='dist'");
+    $result= $query->fetchAll();
+    $count = count($result);
+		if ( $count == '2'){
+			$cmd=("$ROTO/modules/sensors/GPIO/HCS04/hcs04.py");
+			$local_val=$shell_exec($cmd);
+			$device='';
+			$current='';
+			$local_rom='gpio_2324_dist';
+			echo $date." Rom: ".$local_rom." Value:".$local_val."\n";
+			db($local_rom,$local_val,$local_type,$device,$current);
+		} else {
+			echo $date." No distance configured on GPIO 23 && 24.\n";
+		}
+} catch (Exception $e) {
+echo $date." Error.\n";
+echo $e;
+exit;
+}
+?>
