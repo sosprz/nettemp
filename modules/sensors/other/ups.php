c4bb1220fe08802a3d03755a9e8bd4a3d484fba6
admin <admin@techfreak.pl>
2016-12-08 20:32:37 +0100

    pimowo UPS php

---
+++ modules/sensors/other/ups.php
@@ -0,0 +1,46 @@
+<?php
+$ROOT=dirname(dirname(dirname(dirname(__FILE__))));
+$date = date("Y-m-d H:i:s"); 
+define("LOCAL","local");
+
+try {
+    $db = new PDO("sqlite:$ROOT/dbf/nettemp.db");
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo $date." Could not connect to the database.\n";
+    exit;
+}
+
+try {
+    include("$ROOT/receiver.php");
+    $query = $db->query("SELECT dev FROM usb WHERE device='UPS Pimowo'");
+    $result= $query->fetchAll();
+    foreach($result as $r) {
+		$dev=$r['dev'];
+    }
+    if($dev=='none'){
+		echo $date." No UPS Pimowo USB Device.\n";
+		exit;
+	}
+	$dev2=str_replace("/dev/","",$dev);
+	$cmd=("exec 3<$dev && echo \"S\" >$dev && head -1 <&3; exec 3<&-");
+	$out=shell_exec($cmd);;
+	var_dump($out);
+	
+	$device='';
+	$current='';
+	$local_type='volt';
+	$local_rom="usb_".$dev2."_".$local_type;
+	echo $date." UPS Pimowo Volt: ".$out[0]."\n";
+	db($local_rom,$local_val,$local_type,$device,$current);
+	
+	$local_type='amps';
+	$local_rom="usb_".$dev2."_".$local_type;
+	echo $date." UPS Pimowo Amps: ".$out[1]."\n";
+	db($local_rom,$local_val,$local_type,$device,$current);
+} catch (Exception $e) {
+    echo $date." Error.\n";
+    echo $e;
+    exit;
+}
+?>
