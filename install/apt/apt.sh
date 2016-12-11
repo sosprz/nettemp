231b48318cdcc90bf2966775518a53cb34e470f9
admin <admin@techfreak.pl>
2016-12-11 09:27:55 +0100

    installer

--- install/apt/apt.sh
+++ install/apt/apt.sh
@@ -1,22 +1,16 @@
 #! /bin/bash
 
-package=$(cat $dir/install/apt/packages_list)
+list=$(cat $dir/install/apt/packages_list)
+packages=$(cat $list)
 clear
 echo -n -e "Updating repo \r"
-{
-	apt-get -y update
-}  >> $dir/install_log.txt 2>&1
+apt-get -y update >> $dir/install_log.txt 2>&1
 
-for i in $package
-    do
-    clear
-    echo -n -e "Installing $i \r"
-    { 
-	apt-get -y install $i 
-    } >> $dir/install_log.txt 2>&1
-done
+clear
+echo -n -e "Instaling packages \r"
+apt-get -y install $packages >> $dir/install_log.txt 2>&1
 
-
+clear
 for i in $package; do
     dpkg-query -W -f='${Package}\t\t${Status}' $i 
     exitstatus=$?
@@ -28,41 +22,6 @@ for i in $package; do
     exit
     fi
 done
-
-exitstatus=$?
-if [ $exitstatus = 1 ]; then
-    echo -e "[ ${RED}error${R} ] packages"
-    exit 1
-else 
-    echo -e "[ ${GREEN}ok${R} ] packages"
-fi
-
-
-
-
-
-
-echo "Please wait installing packages"
-package=$($dir/install/apt/packages_list)
-
-{
-package=$(cat packages_list)
-apt-get -y update
-apt-get -y install $package
-} >> $dir/install_log.txt 2>&1
-
-for i in $package; do
-    dpkg-query -W -f='${Package}\t\t${Status}' $i 
-    exitstatus=$?
-    echo -e
-    if [ $exitstatus = '1' ]; then
-    echo -e No package $i  >> $dir/install_log.txt 2>&1
-    echo -e No package $i
-    echo -e "[ ${RED}error${R} ] packages"
-    exit
-    fi
-done
-
 
 exitstatus=$?
 if [ $exitstatus = 1 ]; then
