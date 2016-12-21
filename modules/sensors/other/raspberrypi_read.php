c9e33925c82509bc748062cd5718cc60f3a05d27
admin <admin@techfreak.pl>
2016-12-21 07:50:52 +0100

    rpi

--- modules/sensors/other/raspberrypi_read.php
+++ modules/sensors/other/raspberrypi_read.php
@@ -12,8 +12,29 @@ if(file_exists("/opt/vc/bin/vcgencmd")) {
 		$local_val = $output;
 		$local_type = 'temp';
 		$local_rom = "Raspberry_Pi";
+		$local_device = 'rpi';
 		echo $date." ".$local_rom." ".$local_val."\n";
 		db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 	}
 }
+
+
+if(file_exists("/sys/devices/platform/sunxi-i2c.0/i2c-0/0-0034/temp1_input")) {
+	$cmd = "cat /sys/devices/platform/sunxi-i2c.0/i2c-0/0-0034/temp1_input";
+	$output = shell_exec($cmd);
+    $output = trim($output)/1000;
+    if(is_numeric($output)) {
+		include("$ROOT/receiver.php");
+		$output = number_format($output, 2, '.', '');
+		$local_rom = "Banana_Pi";
+		$local_val = $output;
+		$local_type = 'temp';
+		$local_device = 'banana';
+		echo $date." ".$local_rom." ".$local_val."\n";
+		db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
+	}
+}
+
+
+
 ?>
