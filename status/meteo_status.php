586d9eb139bcd5f3c4f621fe8c24fc80597ebf6c
gjot <gjot@x.xxx>
2016-12-31 15:48:10 +0000

    fixed require error in meteo_status

--- status/meteo_status.php
+++ status/meteo_status.php
@@ -12,9 +12,9 @@ foreach ($row as $a) {
     $onoff=$a["onoff"];
     }
 
-if ( $onoff == "on") { 
+if ( $onoff == "on") {
 
-require_once("status/Meteo.class.php"); 
+require_once($root."/status/Meteo.class.php"); 
 $meteo=new Meteo();
 $sila_grawitacji=$meteo->getSilaGrawitacji();//9.780313*(pow(1+0.005324*SIN($szerokosc),2)-0.0000058*pow(SIN(2*$szerokosc),2)-0.000003085*$wysokosc);
 $temp_znormalizowana=$meteo->getTemperaturaZnormalizowana();//((2*($temperatura+273.15))+((0.6*$wysokosc)/100))/2;
@@ -39,8 +39,8 @@ $wb=$meteo->getWb();//2165*(($cisnienie_pary/10)/(273.15+$temperatura));
 <table class="table stripped table-condensed small">
 
 <tr>
-    <td>Sila grawitacji [m/s^2]"</td>
-    <td><?php 
+    <td>Sila grawitacji [m/s^2]</td>
+    <td><?php
 	    //$sila_grawitacji=9.780313*(pow(1+0.005324*SIN($szerokosc),2)-0.0000058*pow(SIN(2*$szerokosc),2)-0.000003085*$wysokosc);
 	    echo number_format($sila_grawitacji, 2, '.', '');
 	?>
@@ -50,7 +50,7 @@ $wb=$meteo->getWb();//2165*(($cisnienie_pary/10)/(273.15+$temperatura));
 <tr>
     <td>Temperatura znormalizowana [K]
     <td>
-	<?php 
+	<?php
 	    //$temp_znormalizowana=((2*($temperatura+273.15))+((0.6*$wysokosc)/100))/2;
 	    echo number_format($temp_znormalizowana, 2, '.', '');
 	?>
@@ -60,7 +60,7 @@ $wb=$meteo->getWb();//2165*(($cisnienie_pary/10)/(273.15+$temperatura));
 <tr>
     <td>Temperatura znormalizowana [C]
     <td>
-	<?php 
+	<?php
 	    //$tz=$temp_znormalizowana-273.15;
 	    echo number_format($tz, 2, '.', '');
 	?>
@@ -70,7 +70,7 @@ $wb=$meteo->getWb();//2165*(($cisnienie_pary/10)/(273.15+$temperatura));
 <tr>
     <td>Cisnienie znormalizowane [hPa]
     <td>
-	<?php 
+	<?php
 	    //$cz=($cisnienie*(EXP(($sila_grawitacji*$wysokosc)/(287.05*$temp_znormalizowana)))*10)/10;
 	    echo number_format($cz, 2, '.', '');
 	?>
@@ -81,7 +81,7 @@ $wb=$meteo->getWb();//2165*(($cisnienie_pary/10)/(273.15+$temperatura));
 <tr>
     <td>Temperatura punktu rosy [°C]
     <td>
-	<?php 
+	<?php
 	    //$tpr=243.12*(((LOG10($wilgotnosc)-2)/0.4343)+(17.5*$temperatura)/(243.12+$temperatura))/(17.62-(((LOG10($wilgotnosc)-2)/0.4343)+(17.5*$temperatura)/(243.12+$temperatura)));
 	    echo number_format($tpr, 2, '.', '');
 	?>
@@ -92,7 +92,7 @@ $wb=$meteo->getWb();//2165*(($cisnienie_pary/10)/(273.15+$temperatura));
 <tr>
     <td>Cisnienie pary wodnej nasyconej [hPa]
     <td>
-	<?php 
+	<?php
 	    //$cisnienie_pary_nasyconej=6.112*EXP((17.67*$temperatura)/($temperatura+243.5));
 	    echo number_format($cisnienie_pary_nasyconej, 2, '.', '');
 	?>
@@ -102,7 +102,7 @@ $wb=$meteo->getWb();//2165*(($cisnienie_pary/10)/(273.15+$temperatura));
 <tr>
     <td>Cisnienie pary wodnej [hPa]
     <td>
-	<?php 
+	<?php
 	    //$cisnienie_pary=$wilgotnosc/(1/$cisnienie_pary_nasyconej)/100;
 	    echo number_format($cisnienie_pary, 2, '.', '');
 	?>
@@ -111,7 +111,7 @@ $wb=$meteo->getWb();//2165*(($cisnienie_pary/10)/(273.15+$temperatura));
 <tr>
     <td>Wilgotnosc bezwzgledna [g/m^3]
     <td>
-	<?php 
+	<?php
 	    //$wb=2165*(($cisnienie_pary/10)/(273.15+$temperatura));
 	    echo number_format($wb, 2, '.', '');
 	?>
