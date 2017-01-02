2673642e2bf82e06b7068c8276209280f6be24cf
admin <admin@techfreak.pl>
2017-01-02 23:08:53 +0100

    ds test

--- modules/sensors/i2c/i2c_read.php
+++ modules/sensors/i2c/i2c_read.php
@@ -144,6 +144,7 @@ function read($addr,$name,$bus){
 		shell_exec("sudo cp $ROOT/install/1w/nettemp.ds2482 /etc/init.d/");
 		shell_exec("sudo update-rc.d nettemp.ds2482 defaults");
 		shell_exec("sudo service nettemp.ds2482 start");
+		echo $date." ".$dsfile." created";
 	}
 
 }
