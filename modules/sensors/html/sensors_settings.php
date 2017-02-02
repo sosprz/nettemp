6f447f577c34baed6d6322df37525a024fe0006e
admin <admin@techfreak.pl>
2017-02-02 23:23:41 +0100

    remove buttons form settings

--- modules/sensors/html/sensors_settings.php
+++ modules/sensors/html/sensors_settings.php
@@ -445,7 +445,8 @@ $row = $rows->fetchAll();
     <td class="col-md-0">
     <form action="" method="post" style="display:inline!important;" > 	
 		<input type="hidden" name="charts" value="<?php echo $a["id"]; ?>" />
-		<input type="checkbox" data-toggle="toggle" data-size="mini"  name="chartson" value="on" <?php echo $a["charts"] == 'on' ? 'checked="checked"' : ''; ?> onchange="this.form.submit()" />
+		<button type="submit" name="chartson" value="<?php echo $a["charts"] == 'on' ? 'off' : 'on'; ?>" <?php echo $a["charts"] == 'on' ? 'class="btn btn-xs btn-danger"' : 'class="btn btn-xs btn-success"'; ?>>
+	    <?php echo $a["charts"] == 'on' ? '<span class="glyphicon glyphicon-off" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-play" aria-hidden="true"></span>'; ?></button>
 		<input type="hidden" name="chartsonoff" value="onoff" />
     </form>
     </td>
@@ -454,7 +455,8 @@ $row = $rows->fetchAll();
     <?php if ($a["device"] != 'remote') { ?>
     <form action="" method="post" style="display:inline!important;"> 	
 		<input type="hidden" name="remote" value="<?php echo $a["id"]; ?>" />
-		<input type="checkbox" data-toggle="toggle" data-size="mini"  name="remoteon" value="on" <?php echo $a["remote"] == 'on' ? 'checked="checked"' : ''; ?>   onchange="this.form.submit()" />
+		<button type="submit" name="remoteon" value="<?php echo $a["remote"] == 'on' ? 'off' : 'on'; ?>" <?php echo $a["remote"] == 'on' ? 'class="btn btn-xs btn-danger"' : 'class="btn btn-xs btn-success"'; ?>>
+	    <?php echo $a["remote"] == 'on' ? '<span class="glyphicon glyphicon-off" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-play" aria-hidden="true"></span>'; ?></button>
 		<input type="hidden" name="remoteonoff" value="onoff" />
     </form>
     <?php 
@@ -465,7 +467,8 @@ $row = $rows->fetchAll();
     <td class="col-md-0">
     <form action="" method="post" style="display:inline!important;"> 	
 		<input type="hidden" name="minmax" value="<?php echo $a["id"]; ?>" />
-		<input type="checkbox" data-toggle="toggle" data-size="mini"  name="minmaxon" value="on" <?php echo $a["minmax"] == 'on' ? 'checked="checked"' : ''; ?> onchange="this.form.submit()" />
+		<button type="submit" name="minmaxon" value="<?php echo $a["minmax"] == 'on' ? 'off' : 'on'; ?>" <?php echo $a["minmax"] == 'on' ? 'class="btn btn-xs btn-danger"' : 'class="btn btn-xs btn-success"'; ?>>
+	    <?php echo $a["minmax"] == 'on' ? '<span class="glyphicon glyphicon-off" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-play" aria-hidden="true"></span>'; ?></button>
 		<input type="hidden" name="minmaxonoff" value="onoff" />
     </form>
     </td>
@@ -473,7 +476,8 @@ $row = $rows->fetchAll();
     <td class="col-md-0">
     <form action="" method="post" style="display:inline!important;"> 	
 		<input type="hidden" name="lcdid" value="<?php echo $a["id"]; ?>" />
-		<input type="checkbox" data-toggle="toggle" data-size="mini"  name="lcdon" value="on" <?php echo $a["lcd"] == 'on' ? 'checked="checked"' : ''; ?> onchange="this.form.submit()" />
+		<button type="submit" name="lcdon" value="<?php echo $a["lcd"] == 'on' ? 'off' : 'on'; ?>" <?php echo $a["lcd"] == 'on' ? 'class="btn btn-xs btn-danger"' : 'class="btn btn-xs btn-success"'; ?>>
+	    <?php echo $a["lcd"] == 'on' ? '<span class="glyphicon glyphicon-off" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-play" aria-hidden="true"></span>'; ?></button>
 		<input type="hidden" name="lcd" value="lcd" />
     </form>
     </td>
@@ -481,7 +485,8 @@ $row = $rows->fetchAll();
     <td class="col-md-0">
     <form action="" method="post" style="display:inline!important;"> 	
 		<input type="hidden" name="jgid" value="<?php echo $a["id"]; ?>" />
-		<input type="checkbox" data-toggle="toggle" data-size="mini"  name="jgon" value="on" <?php echo $a["jg"] == 'on' ? 'checked="checked"' : ''; ?> onchange="this.form.submit()" />
+		<button type="submit" name="jgon" value="<?php echo $a["jg"] == 'on' ? 'off' : 'on'; ?>" <?php echo $a["jg"] == 'on' ? 'class="btn btn-xs btn-danger"' : 'class="btn btn-xs btn-success"'; ?>>
+	    <?php echo $a["jg"] == 'on' ? '<span class="glyphicon glyphicon-off" aria-hidden="true"></span>' : '<span class="glyphicon glyphicon-play" aria-hidden="true"></span>'; ?></button>
 		<input type="hidden" name="jg" value="jg" />
     </form>
     </td>
