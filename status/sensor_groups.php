ec2ad773b2b6687cd49c68de4c174dc0a770078f
admin <admin@techfreak.pl>
2017-04-09 15:04:21 +0200

    add single chart max

--- status/sensor_groups.php
+++ status/sensor_groups.php
@@ -107,7 +107,7 @@ foreach ($row_meteo as $a) {
 				?>
 			</td>
 			<td>
-			    <a href="index.php?id=view&type=<?php echo $a['type']?>&max=day&single=<?php echo $a['name']?>" title="Go to charts, last update: <?php echo $a['time']?>"
+			    <a href="index.php?id=view&type=<?php echo $a['type']?>&max=<?php echo $nts_charts_max ?>&single=<?php echo $a['name']?>" title="Go to charts, last update: <?php echo $a['time']?>"
 				<?php 
 					$old_read=86400;
 				    if (($a['tmp'] == 'error') || ($a['status'] == 'error') || ($label=='danger') || strtotime($a['time'])<(time()-(7*$old_read))){
