b10481cd7f6940062412bfebf3af85d203ecf9ff
gjot <gjot@users.noreply.github.com>
2017-05-10 19:51:36 +0000

    MultiLCD DB changes

--- modules/tools/update_db_new.php
+++ modules/tools/update_db_new.php
@@ -27,4 +27,10 @@ $updates['2017-05-04 20:00:00'][]="ALTER TABLE newdev ADD type TEXT";
 $updates['2017-05-04 20:00:00'][]="ALTER TABLE newdev ADD usb  TEXT";
 $updates['2017-05-04 20:00:00'][]="ALTER TABLE newdev ADD seen  TEXT";
 
+//MultiLCD DB changes
+$updates['2017-05-10 19:25:15'][]="CREATE TABLE lcds (id INTEGER PRIMARY KEY, name TEXT NOT NULL, addr TEXT NOT NULL UNIQUE, rows TINYINT NOT NULL DEFAULT 2, cols TINYINT NOT NULL DEFAULT 16, clock TEXT DEFAULT '', avg TEXT DEFAULT '', active TEXT DEFAULT 'on', grp TEXT DEFAULT NULL, loop TEXT DEFAULT '')";
+$updates['2017-05-10 19:25:15'][]="CREATE TABLE lcd_group_assign (rom TEXT NOT NULL, grpkey TEXT NOT NULL)";
+$updates['2017-05-10 19:25:15'][]="CREATE TABLE lcd_groups (id INTEGER PRIMARY KEY, name TEXT UNIQUE, active TEXT DEFAULT 'on', charts TEXT DEFAULT '', grpkey TEXT UNIQUE DEFAULT (lower(hex(randomblob(4)))) NOT NULL)";
+$updates['2017-05-10 19:25:15'][]="INSERT INTO nt_settings ('option', 'value') VALUES ('lcdmode','off')";
+
 ?>
