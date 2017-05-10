1808967bce083ec849632cb3cf4254c303d141c7
gjot <gjot@users.noreply.github.com>
2017-05-10 19:23:45 +0000

    FIX: settings cannot be obtained in certain conditions

--- modules/settings/nt_settings.php
+++ modules/settings/nt_settings.php
@@ -1,5 +1,13 @@
 <?php
-$root=$_SERVER["DOCUMENT_ROOT"];
+if(!empty($_SERVER["DOCUMENT_ROOT"])){
+    $root=$_SERVER["DOCUMENT_ROOT"];
+}else{
+    $root=__DIR__;
+    for($i=0;$i<5;$i++){
+        $root = file_exists($root.'/dbf/nettemp.db') ? $root : dirname($root) ;
+    }
+}
+
 $db = new PDO("sqlite:$root/dbf/nettemp.db");
 $sth = $db->query("SELECT * FROM nt_settings");
 $sth->execute();
