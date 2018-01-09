c8094a1a738450ca95e2a652d256cfaa1b13bace
Mariusz Musik <m@serwer-robelit.local>
2018-01-09 17:16:16 +0100

    rom

--- modules/gpio/temp.php
+++ modules/gpio/temp.php
@@ -209,7 +209,7 @@ foreach ($row as $a) {
 	$day_run=$a['day_run'];
 	$state=$a['state'];
 	
-	$rom=$a['rom'];
+	//$rom=$a['rom'];
 			
 	$rows = $db->query("SELECT * FROM g_func WHERE gpio='$gpio' ORDER BY position ASC");
 	$func = $rows->fetchAll();
