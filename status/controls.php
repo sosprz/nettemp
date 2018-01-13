24f543e78ec019433fe3c7ea175608656099159b
Mariusz Musik <m@serwer-robelit.local>
2018-01-13 16:01:05 +0100

    time

--- status/controls.php
+++ status/controls.php
@@ -407,7 +407,7 @@ if(!empty($ip_gpio)||!empty($sensors_relay)) {
 					<?php
 					if (substr($g['status'],0,2) == 'ON') {
 					?>
-						<span class="label <?php echo label($g['status']) ?>"> <?php echo (str_replace"m","",substr($g['status'],3,3))." min" ?> </span>
+						<span class="label <?php echo label($g['status']) ?>"> <?php echo str_replace("m","",substr($g['status'],3,3))." min" ?> </span>
 					</td>
 					<?php 
 					}
