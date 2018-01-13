bf0a64f17551cbd2f621b52b3c1a0f3836f987c9
Mariusz Musik <m@serwer-robelit.local>
2018-01-13 13:49:11 +0100

    time

--- status/controls.php
+++ status/controls.php
@@ -158,7 +158,7 @@ if(!empty($ip_gpio)||!empty($sensors_relay)) {
 <div class="panel-heading">
 <h3 class="panel-title">GPIO</h3>
 </div>
-<table class="table table-hover table-condensed small" border="0">
+<table class="table table-hover table-condensed small vertical-align: middle" border="0">
 	<tbody>
 	<?php
 	/* EasyESP */
