3d64d6f7e922c7f82cd787b8a61e9929e9d6870e
Mariusz Musik <m@serwer-robelit.local>
2018-01-17 14:39:14 +0100

    minmax

--- receiver.php
+++ receiver.php
@@ -249,13 +249,10 @@ function db($rom,$val,$type,$device,$current,$ip,$gpio,$i2c,$usb,$name){
 					//sensors status
 					else {
 						$dbr->exec("UPDATE sensors SET tmp='$val', status='ok', ip='$ip' WHERE rom='$rom'") or die (date("Y-m-d H:i:s")." ERROR: Cannot insert value to status\n" );
-						
+						//minmax light
 						if ($val<$stat_min || empty($stat_min)) {$dbr->exec("UPDATE sensors SET stat_min='$val' WHERE rom='$rom'");
-						
 						} elseif ($val>$stat_max || empty($stat_max)) {$dbr->exec("UPDATE sensors SET stat_max='$val' WHERE rom='$rom'");}
 						
-						//$dbr->exec("UPDATE sensors SET stat_min='$val' WHERE stat_min>'$val' AND rom='$rom' OR stat_min is null AND rom='$rom'");
-						//$dbr->exec("UPDATE sensors SET stat_max='$val' WHERE stat_max<'$val' AND rom='$rom' OR stat_max is null AND rom='$rom'");
 						if(!is_null($ip)&&$device=='gpio') {
 						    $dbr->exec("UPDATE gpio SET ip='$ip' WHERE rom='$rom'") or die (date("Y-m-d H:i:s")." ERROR: Cannot insert IP to gpio\n" );
 						}
