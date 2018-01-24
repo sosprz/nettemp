0279c587fa83831a828c5636d69cf5b3604d37c2
mariano78 <mariano78@interia.pl>
2018-01-24 14:01:38 +0100

    test

--- modules/sensors/GPIO/DHT/dht_read.php
+++ modules/sensors/GPIO/DHT/dht_read.php
@@ -33,11 +33,12 @@ try {
 		
 		if(!empty($humid)){
 			$local_val=$humid;
+			$local_val2=$temp;
 			$local_type='humid';
 			$local_device='gpio';
 		    $local_gpio=$gpio;
 			$local_rom="gpio_".$gpio."_".$local_type;
-			echo $date." Rom: ".$local_rom." Value:".$local_val."\n";
+			echo $date." Rom: ".$local_val2." Value:".$local_val."\n";
 			db($local_rom,$local_val,$local_type,$local_device,$local_current,$local_ip,$local_gpio,$local_i2c,$local_usb,$local_name);
 		}
 		
