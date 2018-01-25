446573fbc0f53680fea86cd84ae47cf05b6f3dca
mariano78 <mariano78@interia.pl>
2018-01-25 11:12:17 +0100

    dp

--- status/controls.php
+++ status/controls.php
@@ -359,7 +359,7 @@ if(!empty($ip_gpio)||!empty($sensors_relay)) {
 				   <td>
 					<form class="form-horizontal" action="" method="post" style=" display:inline!important;">
 						<input type="hidden" name="gpio_lock_update_from_status" value="<?php echo $s['gpio']; ?>"/>
-						<input id="lockstatus" type="checkbox"  data-toggle="toggle" data-size="mini" data-on="lock" data-off="lock" onchange="this.form.submit()" name="lock_update_from_status" value="<?php echo $g['locked'] == 'user'  ? '' : 'user'; ?>" <?php echo $g['locked'] == 'user' ? 'checked="checked"' : ''; ?>  />
+						<input id="lockstatus" type="checkbox"  data-toggle="toggle" data-size="mini" data-on="lock" data-off="lock" class="btn btn-warning" onchange="this.form.submit()" name="lock_update_from_status" value="<?php echo $g['locked'] == 'user'  ? '' : 'user'; ?>" <?php echo $g['locked'] == 'user' ? 'checked="checked"' : ''; ?>  />
 						<input type="hidden" name="rom_lock" value="<?php echo $s['rom']; ?>"/>
 						<input type="hidden" name="update_from_status" value="lock_update_from_status" />
 					</form>
