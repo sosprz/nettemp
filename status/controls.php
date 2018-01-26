ac5114f33ada43197216797aff6c9ab750b2836b
mariano78 <mariano78@interia.pl>
2018-01-26 10:10:55 +0100

    DP status

--- status/controls.php
+++ status/controls.php
@@ -607,7 +607,7 @@ if(!empty($ip_gpio)||!empty($sensors_relay)) {
 				/* TEMP */
 				if($g['mode']=='temp') {
 					/* TEMP */
-					$sth = $db->prepare("SELECT id,value FROM g_func WHERE gpio='$s[gpio]' ORDER BY position ASC LIMIT 1 ");
+					$sth = $db->prepare("SELECT id,value FROM g_func WHERE gpio='$s[gpio]' AND active='on' ORDER BY position ASC LIMIT 1 ");
 					$sth->execute();
 					$g_func = $sth->fetchAll();
 					//$numRows = count($result);
