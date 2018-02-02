53f7b933b196b9dba131e4bf5be773659c91c199
mariano78 <mariano78@interia.pl>
2018-02-02 09:48:54 +0100

    Sensors logon

--- status/sensor_groups.php
+++ status/sensor_groups.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 if (isset($_GET['ch_g'])) { 
     $ch_g = $_GET['ch_g'];
 } 
@@ -9,6 +11,9 @@ $db = new PDO("sqlite:$root/dbf/nettemp.db");
 $query = $db->query("SELECT * FROM types");
 $result_t = $query->fetchAll();
 
+
+
+
 $rows_meteo = $db->query("SELECT normalized,pressure FROM meteo WHERE id='1'");
 $row_meteo = $rows_meteo->fetchAll();
 foreach ($row_meteo as $a) {
@@ -16,7 +21,7 @@ foreach ($row_meteo as $a) {
 	$pressure=$a['pressure'];
 }
 
-	if(isset($_SESSION["user"])) {
+	if(($_SESSION["perms"] == 'adm') || (isset($_SESSION["user"]))) {
 
 		$sth = $db->prepare("SELECT * FROM sensors WHERE position !=0 AND ch_group='$ch_g' AND type!='gpio' AND type!='elec' AND type!='water' AND type!='gas' AND ch_group!='switch' AND ch_group!='relay' AND (jg!='on' OR jg is null) ORDER BY position ASC");
 	} else {
