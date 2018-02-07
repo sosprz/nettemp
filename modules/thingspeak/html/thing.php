266f84c13df80bd6d23ea0a73bc980cf87bc6927
mariano78 <mariano78@interia.pl>
2018-02-07 15:46:41 +0100

    th footer

--- modules/thingspeak/html/thing.php
+++ modules/thingspeak/html/thing.php
@@ -262,20 +262,15 @@ foreach ($row as $a) {
 
 <div class="panel panel-default">
   <div class="panel-heading">Info:</div>
-	
-	
-	<div class="table-responsive">
-	<table class="table table-hover table-condensed small" border="0">
-		<tr></td class="col-md-12">
-		<span>
+	<div class="panel-body">
+
+		<span id="helpBlock" class="help-block">
 			ThingSpeak is available as a free service for small non-commercial home projects - 3 million messages/year. <br>
 			Recommended maximum daily usage capacity:  8 219 messages. <br>
 			For questions about exceeding suggested daily usage rate or for purchasing new units,
 			see the
 			<a href="https://thingspeak.com/pages/license_faq" target="_blank">Licensing FAQ</a>
-		</span>
-		</td></tr>
 		
-		</table>
+		</span>
 	</div>
 </div>
