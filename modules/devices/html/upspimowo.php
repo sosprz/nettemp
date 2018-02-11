8f4d7f3527219f2f98f921cf41882bd155270023
mariano78 <mariano78@interia.pl>
2018-02-11 08:51:32 +0100

    ups

--- modules/devices/html/upspimowo.php
+++ modules/devices/html/upspimowo.php
@@ -1,3 +1,33 @@
 <?php
 echo 'pimowo'
 ?>
+
+
+<div class="panel panel-default">
+<div class="panel-heading">UPS Pimowo Settings
+
+
+
+<div class="table-responsive">
+<table class="table table-hover table-condensed small" border="0">
+<thead>
+<tr><
+
+<th>Actual settings</th>
+<th>New settings</th>
+
+</tr>
+
+
+
+
+</thead>
+</table
+
+
+<?php
+
+
+
+?>
+</div>
