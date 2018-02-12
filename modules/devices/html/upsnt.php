ce434133ca5689cfb86a924515c95bf8d93c6bcb
mariano78 <mariano78@interia.pl>
2018-02-12 09:33:41 +0100

    u

--- modules/devices/html/upsnt.php
+++ modules/devices/html/upsnt.php
@@ -128,6 +128,141 @@
 </tr>
 
 
+</thead>
+</table
+
+
+<?php
+
+?>
+</div></div>
+
+<div class="grid-item">
+<div class="panel panel-default">
+<div class="panel-heading">UPS NT Settings</div>
+
+
+
+<div class="table-responsive">
+<table class="table table-hover table-condensed small" border="0">
+<thead>
+<tr>
+
+<th>Settings</th>
+<th>Actual</th>
+<th>New</th>
+</tr>
+
+<tr>
+<td class="col-md-2">DC Input</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">DC Output</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">DC Akku.</td>
+<td></td>
+<td></td>
+</tr>
+
+
+<tr>
+<td class="col-md-2">Current output</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">Power Output</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">Akku. Temp</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">Akku. Capacity</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">Akku. Charge</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">UPS Power</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">Akku. Health</td>
+<td></td>
+<td></td>
+</tr>
+
+
+
+<tr>
+<td class="col-md-2">Delay ON</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">Delay OFF</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">Akku. charge start</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">Akku. charge stop</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">Akku. discharged</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">LCD Scrolling</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td class="col-md-2">LCD Auto Backlight</td>
+<td></td>
+<td></td>
+</tr>
+
+<tr>
+<td></td>
+<td><button type="button" class="btn btn-xs btn-info">Read</button></td>
+<td><button type="button" class="btn btn-xs btn-danger">Save</button></td>
+</tr>
 
 
 </thead>
