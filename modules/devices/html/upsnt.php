11ad93a439fc6175d402da834fc678ddcd8802dc
mariano78 <mariano78@interia.pl>
2018-02-12 10:45:08 +0100

    u

--- modules/devices/html/upsnt.php
+++ modules/devices/html/upsnt.php
@@ -1,145 +1,46 @@
 
 
 <div class="grid">
-<div class="grid-item">
-<div class="panel panel-default">
-<div class="panel-heading">UPS NT Status</div>
-<table class="table table-hover table-condensed">
+		<div class="grid-item">
+				<div class="panel panel-default">
 
-<tbody>
-<tr>
-<td >DC Input</td>
-<td></td>
-<td></td>
-</tr>
+							<div class="panel-heading">UPS NT Status</div>
+								<table class="table table-hover table-condensed">
 
-<tr>
-<td >DC Output</td>
-<td></td>
-<td></td>
-</tr>
+										<tbody>
+												<tr>
+												<td >DC Input</td>
+												<td></td>
+												<td></td>
+												</tr>
 
-<tr>
-<td >DC Akku.</td>
-<td></td>
-<td></td>
-</tr>
+										</tbody>
+								</table>
 
+				</div>
+		</div>
+		
+		
+		
+		
+		<div class="grid-item">
+				<div class="panel panel-default">
 
-<tr>
-<td >Current output</td>
-<td></td>
-<td></td>
-</tr>
+							<div class="panel-heading">UPS NT Status</div>
+								<table class="table table-hover table-condensed">
 
-<tr>
-<td >Power Output</td>
-<td></td>
-<td></td>
-</tr>
+										<tbody>
+												<tr>
+												<td >DC Input</td>
+												<td></td>
+												<td></td>
+												</tr>
 
-<tr>
-<td >Akku. Temp</td>
-<td></td>
-<td></td>
-</tr>
+										</tbody>
+								</table>
 
-<tr>
-<td >Akku. Capacity</td>
-<td></td>
-<td></td>
-</tr>
+				</div>
+		</div>
 
-<tr>
-<td >Akku. Charge</td>
-<td></td>
-<td></td>
-</tr>
-
-<tr>
-<td >UPS Power</td>
-<td></td>
-<td></td>
-</tr>
-
-<tr>
-<td >Akku. Health</td>
-<td></td>
-<td></td>
-</tr>
-</tbody>
-</table>
-
-</div></div>
-
-<div class="grid-item">
-<div class="panel panel-default">
-<div class="panel-heading">UPS NT Settings</div>
-
-<table class="table table-hover table-condensed">
-<thead>
-<tr>
-
-<th>Settings</th>
-<th>Actual</th>
-<th>New</th>
-</tr>
-</thead>
-<tbody>
-
-
-<tr>
-<td >Delay ON</td>
-<td></td>
-<td></td>
-</tr>
-
-<tr>
-<td >Delay OFF</td>
-<td></td>
-<td></td>
-</tr>
-
-<tr>
-<td >Akku. charge start</td>
-<td></td>
-<td></td>
-</tr>
-
-<tr>
-<td >Akku. charge stop</td>
-<td></td>
-<td></td>
-</tr>
-
-<tr>
-<td >Akku. discharged</td>
-<td></td>
-<td></td>
-</tr>
-
-<tr>
-<td >LCD Scrolling</td>
-<td></td>
-<td></td>
-</tr>
-
-<tr>
-<td >LCD Auto Backlight</td>
-<td></td>
-<td></td>
-</tr>
-
-<tr>
-<td></td>
-<td><button type="button" class="btn btn-xs btn-info">Read</button></td>
-<td><button type="button" class="btn btn-xs btn-danger">Save</button></td>
-</tr>
-
-
-</body>
-</table>
-
-</div></div>
 
 </div>
