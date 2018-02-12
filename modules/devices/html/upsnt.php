1d58817a48c87a2a25cb36978253bfca0127345c
mariano78 <mariano78@interia.pl>
2018-02-12 11:02:37 +0100

    u

--- modules/devices/html/upsnt.php
+++ modules/devices/html/upsnt.php
@@ -27,6 +27,61 @@
 												<td></td>
 												</tr>
 
+												<tr>
+												<td >DC Output</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >DC Akku.</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+
+												<tr>
+												<td >Current output</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >Power Output</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >Akku. Temp</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >Akku. Capacity</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >Akku. Charge</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >UPS Power</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >Akku. Health</td>
+												<td></td>
+												<td></td>
+												</tr>
+
 										</tbody>
 								</table>
 
@@ -44,9 +99,51 @@
 
 										<tbody>
 												<tr>
-												<td >DC Input</td>
+												<td >Delay ON</td>
 												<td></td>
 												<td></td>
+												</tr>
+
+												<tr>
+												<td >Delay OFF</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >Akku. charge start</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >Akku. charge stop</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >Akku. discharged</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >LCD Scrolling</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td >LCD Auto Backlight</td>
+												<td></td>
+												<td></td>
+												</tr>
+
+												<tr>
+												<td></td>
+												<td><button type="button" class="btn btn-xs btn-info">Read</button></td>
+												<td><button type="button" class="btn btn-xs btn-danger">Save</button></td>
 												</tr>
 
 										</tbody>
