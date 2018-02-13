2f05a13f6bfa1ca69ad90e9889b6826bbecaebaa
mariano78 <mariano78@interia.pl>
2018-02-13 18:43:55 +0100

    u

--- modules/devices/html/piups.php
+++ modules/devices/html/piups.php
@@ -13,14 +13,7 @@
 
 </style>
 <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
 $root=$_SERVER["DOCUMENT_ROOT"];
-include "php_serial.class.php";
- echo $root;
- $serial = new phpSerial;
 
 $read='zero';
 
