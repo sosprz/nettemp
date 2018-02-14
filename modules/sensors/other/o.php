bc3a36311bdeb9ba53f76ec9bb937a7533c43a81
mariano78 <mariano78@interia.pl>
2018-02-14 13:33:25 +0100

    u

--- modules/sensors/other/o.php
+++ modules/sensors/other/o.php
@@ -2,12 +2,12 @@
 
 $cmd=("exec 3</dev/ttyUSB0 && echo -n '\rO\r' >/dev/ttyUSB0 && head -1 <&3; exec 3<&-");
 $out=shell_exec($cmd);
-$d4=$out;
+//$d4=$out;
     $out=trim($out);
     $data=explode(" ",$out);
-	$d3=$data;
-   var_dump($out);
-   var_dump($data);
+	//$d3=$data;
+   //var_dump($out);
+   //var_dump($data);
    
    
    for($i=0;$i<count($data);$i++){
