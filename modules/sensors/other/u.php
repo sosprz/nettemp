4d0f0b48e22d203a8ee54e1f256254be3ba8f839
mariano78 <mariano78@interia.pl>
2018-02-14 14:51:40 +0100

    u

--- modules/sensors/other/u.php
+++ modules/sensors/other/u.php
@@ -1,11 +1,11 @@
 <?php 
 
 
-$cmd=("echo -n '\rU 61 62 3.9 4.0 3.3 2 11\r' >/dev/ttyUSB0 ");
+$cmd=("echo -n '\rU 67 68 3.9 4.0 3.3 2 11\r' >/dev/ttyUSB0 ");
 
 $out=shell_exec($cmd);
           
-   }
+  
 
 
 
