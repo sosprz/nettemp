1c6e4f368ac588e41d67d22a00904ff6efee9c21
mariano78 <mariano78@interia.pl>
2018-02-14 17:29:05 +0100

    u

--- modules/devices/html/piups.php
+++ modules/devices/html/piups.php
@@ -46,8 +46,12 @@ $savetoups = isset($_POST['savetoups']) ? $_POST['savetoups'] : '';
 	
 	//$values = escapeshellarg($values);
 	
-	$cmd=("echo -n '\r'$values'\r' >/dev/ttyUSB0 ");
-	$out=shell_exec($cmd);
+	//$cmd=("echo -n '\r'$values'\r' >/dev/ttyUSB0 ");
+	//$out=shell_exec($cmd);
+	
+$fp = fopen('/dev/ttyUSB0','r+'); //use this for Linux
+fwrite($fp, "\rU 67 68 3.9 4.0 3.3 3 11\r"); //write string to serial
+fclose($fp);
 	
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
