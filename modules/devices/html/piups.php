b83ea220acd584a2005ac332f93eec3aa442f584
mariano78 <mariano78@interia.pl>
2018-02-16 16:25:20 +0100

    u

--- modules/devices/html/piups.php
+++ modules/devices/html/piups.php
@@ -273,7 +273,7 @@ $row = $rows->fetchAll();
 
 												<tr>
 												<td><span class="label label-default">LCD Scrolling</span></td>
-												<td><span class="label label-success"><?php echo $d8  ?></span></td>
+												<td><span class="label label-success"><?php echo $d7  ?></span></td>
 <td>
 	
 	<input type="text" name="upsscroll" size="2" maxlength="4" value="<?php echo $nts_ups_lcd_scroll; ?>" />
