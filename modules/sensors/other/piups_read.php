f9534de99415b98bfaf1c7c9ceab573941cf5167
mariano78 <mariano78@interia.pl>
2018-02-16 11:22:50 +0100

    u

--- modules/sensors/other/piups_read.php
+++ modules/sensors/other/piups_read.php
@@ -20,6 +20,12 @@ try {
     if($dev=='none'){
     echo $date." No PiUPS USB Device.\n";
     exit;
+	
+	$query = $db->query("SELECT value FROM nt_settings WHERE option='ups_time_off'");
+    $res= $query->fetchAll();
+    foreach($res as $t) {
+     $ttoff=$t['value'];
+    }
     }
     unset($db);
 
@@ -33,7 +39,7 @@ try {
     $data=explode(" ",$out);
     //var_dump($out);
     //var_dump($data);
-	//echo $dev."\n";
+	echo $ttoff."\n";
 	
 	$types=array('volt','volt','volt','amps','watt','temp','battery','trigger','trigger','trigger');
     $echoes=array('UPS Volt IN','UPS Volt Akku','UPS Volt OUT','UPS Amps','UPS Watt','UPS Temp','UPS Battery','UPS Power Trigger','UPS Volt Trigger','UPS Akku Trigger');
