8a252f257af75e7752c6b640ce54802e065b81fd
mariano78 <mariano78@interia.pl>
2018-02-20 09:42:50 +0100

    trig

--- modules/tools/update_db_new.php
+++ modules/tools/update_db_new.php
@@ -86,5 +86,7 @@ $updates['2018-02-19 14:38:00'][]="UPDATE sensors SET trigzero='0' WHERE type='t
 $updates['2018-02-19 14:38:00'][]="UPDATE sensors SET trigone='1' WHERE type='trigger'";
 $updates['2018-02-19 18:54:12'][]="ALTER TABLE sensors ADD trigzeroclr  TEXT";
 $updates['2018-02-19 18:54:12'][]="ALTER TABLE sensors ADD trigoneclr  TEXT";
+$updates['2018-02-20 14:38:00'][]="UPDATE sensors SET trigzeroclr='label-success' WHERE type='trigger'";
+$updates['2018-02-20 14:38:00'][]="UPDATE sensors SET trigoneclr='label-danger' WHERE type='trigger'";
 
 ?>
