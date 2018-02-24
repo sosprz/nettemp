adbcd0c7f353249a70d7b79f722b2cc3c5b55c76
mariano78 <mariano78@interia.pl>
2018-02-24 19:46:39 +0100

    map

--- modules/map/map_settings.php
+++ modules/map/map_settings.php
@@ -1,5 +1,5 @@
 <?php
-include('modules/map/map_settings_sensors.php');
+//include('modules/map/map_settings_sensors.php');
 include('modules/map/map_settings_gpio.php');
 //include('modules/map/map_settings_hosts.php');
 include('modules/map/map_image.php');
