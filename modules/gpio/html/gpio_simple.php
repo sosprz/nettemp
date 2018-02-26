176527c97c32ee7ca7949366283e806100ac160c
mariano78 <mariano78@interia.pl>
2018-02-26 14:53:32 +0100

    Revert "map"
    This reverts commit 6c55c3420390ea0b0579991166d379676dfa1d39.

--- modules/gpio/html/gpio_simple.php
+++ modules/gpio/html/gpio_simple.php
@@ -2,7 +2,7 @@
 
 $simpleon = isset($_POST['simpleon']) ? $_POST['simpleon'] : '';
 if ($simpleon == "on")  {    
-	$db->exec("UPDATE gpio SET simple='on', status='ON' WHERE gpio='$gpio_post' ") or die("PDO exec error");
+	$db->exec("UPDATE gpio SET simple='on', status='ON' WHERE gpio='$gpio_post' AND rom='$rom'") or die("PDO exec error");
     include('gpio_on.php');
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
@@ -11,7 +11,7 @@ if ($simpleon == "on")  {
 	
 $simpleoff = isset($_POST['simpleoff']) ? $_POST['simpleoff'] : '';
 if ($simpleoff == "off")  {
-	$db->exec("UPDATE gpio SET simple='off', status='OFF' WHERE gpio='$gpio_post' ") or die("PDO exec error");
+	$db->exec("UPDATE gpio SET simple='off', status='OFF' WHERE gpio='$gpio_post' AND rom='$rom'") or die("PDO exec error");
     include('gpio_off.php');
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
@@ -21,7 +21,7 @@ if ($simpleoff == "off")  {
 $simpleexit = isset($_POST['simpleexit']) ? $_POST['simpleexit'] : '';
 if (($simpleexit == "simpleexit") ){
     $db = new PDO('sqlite:dbf/nettemp.db') or die("cannot open the database");
-	$db->exec("UPDATE gpio SET mode='' where gpio='$gpio_post' ") or die("simple off db error");
+	$db->exec("UPDATE gpio SET mode='' where gpio='$gpio_post' AND rom='$rom'") or die("simple off db error");
     $db = null;
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
