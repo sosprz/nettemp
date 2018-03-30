9163f370c407a54b21dd96510e9398b7bb3bfa2d
mariano78 <mariano78@interia.pl>
2018-03-30 11:35:14 +0200

    630

--- modules/sensors/rs485/sdm630_get.sh
+++ modules/sensors/rs485/sdm630_get.sh
@@ -5,9 +5,8 @@
 import sys
 
 if len(sys.argv) > 1:
-getusb = sys.argv[1]
-addr = int(sys.argv[2], 16)
-# brate = sys.argv[3]
+    getusb = sys.argv[1]
+    addr = int(sys.argv[2], 16)
 #---------------------------------------------------------------------------# 
 # loading pymodbus modules
 #---------------------------------------------------------------------------# 
