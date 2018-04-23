6f598f23dd287978b839a490cfc38b9230a1b502
mariano78 <mariano78@interia.pl>
2018-04-23 13:19:08 +0200

    remove some features

--- modules/settings/gpio.php
+++ modules/settings/gpio.php
@@ -9,17 +9,6 @@
 		exit();
     }
     
-    $gpiodemo = isset($_POST['gpiodemo']) ? $_POST['gpiodemo'] : '';
-    $gpiodemo_onoff = isset($_POST['gpiodemo_onoff']) ? $_POST['gpiodemo_onoff'] : '';
-    if (($gpiodemo == "onoff") ){
-		$db = new PDO('sqlite:dbf/nettemp.db');
-		$db->exec("UPDATE nt_settings SET value='$gpiodemo_onoff' WHERE option='gpio_demo'") or die ($db->lastErrorMsg());
-		echo $gpio_onoff;
-		header("location: " . $_SERVER['REQUEST_URI']);
-		exit();
-    }
-
-
     $MCP23017_onoff = isset($_POST['MCP23017_onoff']) ? $_POST['MCP23017_onoff'] : '';
     $MCP23017_onoff1 = isset($_POST['MCP23017_onoff1']) ? $_POST['MCP23017_onoff1'] : '';
     if (($MCP23017_onoff1 == "MCP23017_onoff2") ){
@@ -44,17 +33,6 @@
 </div>
 </div>
 
-<div class="panel panel-default">
-<div class="panel-heading">
-<h3 class="panel-title">GPIO demo (fake gpio)</h3>
-</div>
-<div class="panel-body"> 
-<form action="" method="post">
-  <input type="hidden" name="gpiodemo" value="onoff"  />
-  <input data-toggle="toggle" data-size="mini" onchange="this.form.submit()" type="checkbox" name="gpiodemo_onoff" value="on"  <?php echo $nts_gpio_demo == 'on' ? 'checked="checked"' : ''; ?> >
-</form>
-</div>
-</div>
 
 <div class="panel panel-default">
 <div class="panel-heading">
