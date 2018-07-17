81efbbd0d51a36fb041981a059d1d463df7f58ec
mariano78 <mariano78@interia.pl>
2018-07-17 15:25:54 +0200

    Position group fix

--- modules/sensors/html/sensors_settings.php
+++ modules/sensors/html/sensors_settings.php
@@ -280,14 +280,14 @@ if ( $lcd == "lcd"){
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
     }
-    
+    //CHANGE GROUP
     $ch_group = isset($_POST['ch_group']) ? $_POST['ch_group'] : '';
     $ch_grouponoff = isset($_POST['ch_grouponoff']) ? $_POST['ch_grouponoff'] : '';
     $ch_groupon = isset($_POST['ch_groupon']) ? $_POST['ch_groupon'] : '';
 	$ch_position_group = isset($_POST['ch_position_group']) ? $_POST['ch_position_group'] : '';
     if (($ch_grouponoff == "onoff")){
     $db->exec("UPDATE sensors SET ch_group='$ch_groupon' WHERE id='$ch_group'") or die ($db->lastErrorMsg());
-	$db->exec("UPDATE sensors SET position_group='$ch_position_group' WHERE id='$ch_group'") or die ($db->lastErrorMsg());
+	$db->exec("UPDATE sensors SET position_group=(SELECT position_group FROM sensors WHERE ch_group='$ch_position_group') WHERE id='$ch_group'") or die ($db->lastErrorMsg());
     header("location: " . $_SERVER['REQUEST_URI']);
     exit();
     }
@@ -680,7 +680,7 @@ $row = $rows->fetchAll();
 		<option value="none"  <?php echo $a['ch_group'] == 'none' ? 'selected="selected"' : ''; ?>  >none</option>
     </select>
     <input type="hidden" name="ch_grouponoff" value="onoff" />
-	<input type="hidden" name="ch_position_group" value="<?php echo $a['position_group']; ?>" />
+	<input type="hidden" name="ch_position_group" value="<?php echo $a['ch_group']; ?>" />
     <input type="hidden" name="ch_group" value="<?php echo $a['id']; ?>" />
     </form>
     </td>
