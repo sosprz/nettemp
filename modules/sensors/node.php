916e64af442d1302b3879ae29f33b81bc6703db7
mariano78 <mariano78@interia.pl>
2018-08-17 13:15:50 +0200

    Port settings for node

--- modules/sensors/node.php
+++ modules/sensors/node.php
@@ -49,10 +49,10 @@ try {
 			$current=$s['current'];
 			
 			if ($type == 'elec' ){
-				$URL="http://".$client_ip..$client_port."/receiver.php?key=".$client_key."&type=".$type."&rom=".$rom2."&value=".$value."&current=".$current."&device=ip";
+				$URL="http://".$client_ip."".$client_port."/receiver.php?key=".$client_key."&type=".$type."&rom=".$rom2."&value=".$value."&current=".$current."&device=ip";
 			}else
 			{
-				$URL="http://".$client_ip..$client_port."/receiver.php?key=".$client_key."&type=".$type."&rom=".$rom2."&value=".$value."&device=ip";
+				$URL="http://".$client_ip."".$client_port."/receiver.php?key=".$client_key."&type=".$type."&rom=".$rom2."&value=".$value."&device=ip";
 			}
 	
 			$ch = curl_init();
