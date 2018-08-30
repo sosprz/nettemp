0b5ed90c5c28506334866e3fb5b3a0d64f905bc5
Mariusz Musik <m@serwer-robelit.local>
2018-08-30 12:12:27 +0200

    Rpi Identify hardware version

--- modules/tools/html/tools_gpio_readall.php
+++ modules/tools/html/tools_gpio_readall.php
@@ -5,14 +5,16 @@
 <?php
 passthru("/usr/local/bin/gpio readall");
 
+?>
+</pre>
+
+<?php
 $wp = '/usr/local/bin/gpio';
 if (file_exists($wp)) {
 	exec("$wp -v |grep Type:", $wpout );
 	$wpout=$wpout[0];
 	echo '<span class="label label-info">'.$wpout.'</span></br>';
 }
-
 ?>
-</pre>
 </div>
 </div>
