0d0cf67f8a86a77e21fa80d44d89a132319120e5
Mariusz Musik <m@serwer-robelit.local>
2018-08-30 12:09:50 +0200

    Rpi Identify hardware version

--- modules/tools/html/tools_gpio_readall.php
+++ modules/tools/html/tools_gpio_readall.php
@@ -1,9 +1,17 @@
 <div class="panel panel-default">
-<div class="panel-heading">GPIO readall</div>
+<div class="panel-heading">GPIO</div>
 <div class="panel-body">
 <pre>
 <?php
 passthru("/usr/local/bin/gpio readall");
+
+$wp = '/usr/local/bin/gpio';
+if (file_exists($wp)) {
+	exec("$wp -v |grep Type:", $wpout );
+	$wpout=$wpout[0];
+	echo '<span class="label label-info">'.$wpout.'</span></br>';
+}
+
 ?>
 </pre>
 </div>
